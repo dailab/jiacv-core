97dc6fd3e8e1699a04d40856ddd3eabdee59f8bc
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-02-27 11:21:17 +0000

    CHANGE: javadoc
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@12617 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/IAgent.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/IAgent.java
@@ -11,17 +11,21 @@ import de.dailab.jiactng.agentcore.lifecycle.ILifecycle;
 import de.dailab.jiactng.agentcore.lifecycle.ILifecycleListener;
 
 /**
- * Simple agent
+ * Interface for agents. This interfaces defines functionalities, that are
+ * available for other components within and outside of the agent.
  * 
  * @author Joachim Fuchs
+ * @author Thomas Konnerth
  */
 public interface IAgent extends BeanNameAware, ILifecycleListener, Runnable,
     ILifecycle {
 
   /**
-   * Setter for the agentnode that hosts the agent.
+   * Setter for the agentnode that hosts this agent. Called by the agentnode,
+   * when the agent is created.
    * 
    * @param agentNode
+   *          the agentNode that hosts this agent.
    */
   public void setAgentNode(IAgentNode agentNode);
 
@@ -49,16 +53,62 @@ public interface IAgent extends BeanNameAware, ILifecycleListener, Runnable,
    */
   public void setAdaptors(ArrayList adaptors);
 
+  /**
+   * Setter for the name of this agent. Used by Spring during initialisation.
+   * 
+   * @param name
+   *          the name of this agent
+   * @see org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang.String)
+   */
   public void setBeanName(String name);
 
+  /**
+   * Getter for the name of this agent, as set by Spring
+   * 
+   * @return the name of this agent.
+   */
   public String getAgentName();
 
+  /**
+   * Getter for the agentnode that hosts this agent.
+   * 
+   * @return a reference to the agentnode.
+   */
   public IAgentNode getAgentNode();
 
+  /**
+   * Getter for the list of adaptors of this agent.
+   * 
+   * @return the lifelist of the adaptors.
+   */
   public ArrayList<AbstractAgentBean> getAdaptors();
 
+  /**
+   * Getter for the global threadpool responsivle for this agent. All threads
+   * should be created via this threadpool, to make sure the ressources can be
+   * controled. Note that the implementation this method should call the
+   * approriate method of the agentnode to retrieve the threadpool to insure
+   * consistency.
+   * 
+   * @return a new ExecutorService
+   * @see de.dailab.jiactng.agentcore.IAgentNode#getThreadPool()
+   * @see java.util.concurrent.ExecutorService
+   */
   public abstract ExecutorService getThreadPool();
 
+  /**
+   * Getter for a log-instance that heeds the hierarchie of the agentnode and
+   * the agent, i.e the name of the logger consists of the agentnode-name and
+   * the name of the agent-parameter and the beanname. Note that the
+   * implementation this method should call the approriate method of the
+   * agentnode to retrieve the log-instance to insure consistency.
+   * 
+   * @param bean
+   *          the bean for which the logger shall be retrieved.
+   * @return a log-object that contains the agentnodes name, the agents name and
+   *         the beanname.
+   * @see org.apache.commons.logging.Log;
+   */
   public Log getLog(AbstractAgentBean bean);
 
 }
--- agentCore/src/main/java/de/dailab/jiactng/agentcore/IAgentNode.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/IAgentNode.java
@@ -9,30 +9,112 @@ import org.springframework.beans.factory.BeanNameAware;
 import de.dailab.jiactng.agentcore.lifecycle.ILifecycle;
 
 /**
- * Simple agent platform
+ * Interface for agentnodes. This interfaces defines functionalities, that are
+ * available for other components within and outside of the node.
  * 
  * @author Joachim Fuchs
+ * @author Thomas Konnerth
  */
 public interface IAgentNode extends ILifecycle, BeanNameAware {
 
+  /**
+   * Returns the unique identifier for this agentnode. The identifier is created
+   * when the agentnode is first created, and stays fixed as long as the jvm is
+   * alive.
+   * 
+   * @return a String containing the unique identifier for this agentnode.
+   */
   public String getUUID();
 
+  /**
+   * The name of this agentnode, as set by Spring
+   * 
+   * @return the name of this agentnode.
+   */
   public String getName();
 
+  /**
+   * Setter for the name of this agentnode. Used by Spring during
+   * initialisation.
+   * 
+   * @param name
+   *          the name of this agentnode
+   * @see org.springframework.beans.factory.BeanNameAware#setBeanName(java.lang.String)
+   */
   public void setBeanName(String name);
 
+  /**
+   * Setter for the initial list of agents of this agentnode. Used by Spring
+   * during initialisation.
+   * 
+   * @param agents
+   *          the list of agents that should be started with the agendnodes.
+   */
   public void setAgents(ArrayList<IAgent> agents);
 
+  /**
+   * Adds an agent to the agentnode during runtime.
+   * 
+   * @param agent
+   *          the instance if IAgent that shall be started on the agentnode.
+   * @see de.dailab.jiactng.agentcore.IAgent
+   */
   public void addAgent(IAgent agent);
 
+  /**
+   * Removes an agent from the agentnode during runtime. The agents state is set
+   * to cleanup and the agentobject is destroyed afterwards.
+   * 
+   * @param agent
+   *          the instance if IAgent that shall be removed on the agentnode.
+   * @see de.dailab.jiactng.agentcore.IAgent
+   */
   public void removeAgent(IAgent agent);
 
+  /**
+   * Getter for the current list of agents on the agentnode.
+   * 
+   * @return a list of IAgent-instances that are currently residing on this
+   *         agentnode.
+   */
   public ArrayList<IAgent> findAgents();
 
+  /**
+   * Getter for a log-instance that heeds the hierarchie of the agentnode, i.e
+   * the name of the logger consists of the agentnode-name and the name of the
+   * agent-parameter.
+   * 
+   * @param agent
+   *          the agent for which the logger shall be instantiated.
+   * @return a log-object that contains the agentnodes name and the agents name.
+   * @see org.apache.commons.logging.Log;
+   */
   public Log getLog(IAgent agent);
 
+  /**
+   * Getter for a log-instance that heeds the hierarchie of the agentnode and
+   * the agent, i.e the name of the logger consists of the agentnode-name and
+   * the name of the agent-parameter and the beanname.
+   * 
+   * 
+   * @param agent
+   *          the agent that contains the bean for which the logger shall be
+   *          instantiated.
+   * @param bean
+   *          the bean for which the logger shall be instantiated.
+   * @return a log-object that contains the agentnodes name, the agents name and
+   *         the beanname.
+   * @see org.apache.commons.logging.Log;
+   */
   public Log getLog(IAgent agent, AbstractAgentBean bean);
 
+  /**
+   * Getter for the global threadpool for this agentnode. All threads should be
+   * created via this threadpool, to make sure the ressources can be controled.
+   * 
+   * @return a new ExecutorService
+   * @see java.util.concurrent.ExecutorService
+   */
   public ExecutorService getThreadPool();
 
 }

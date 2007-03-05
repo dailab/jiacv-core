6956b0986c29b18e81ed745cb55a98c399957b7a
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-03-05 10:33:13 +0000

    NEW: added first version of Sensor/Effector interfaces and Actions
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@12630 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/action/IAction.java
@@ -0,0 +1,12 @@
+/*
+ * Created on 27.02.2007
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - Java - Code Generation - Code and Comments
+ */
+package de.dailab.jiactng.agentcore.action;
+
+
+public interface IAction {
+
+}

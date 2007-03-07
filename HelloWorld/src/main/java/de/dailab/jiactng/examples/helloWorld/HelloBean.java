b7bf61b05e3e3fddd642701edcf1a75d288ea0c1
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-03-07 17:11:31 +0000

    ADDED example module ADDED HelloWorld example ADDED PingPong example
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@12672 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ HelloWorld/src/main/java/de/dailab/jiactng/examples/helloWorld/HelloBean.java
@@ -0,0 +1,38 @@
+/*
+ * Created on 20.02.2007
+ */
+package de.dailab.jiactng.examples.helloWorld;
+
+import de.dailab.jiactng.agentcore.AbstractAgentBean;
+import de.dailab.jiactng.agentcore.lifecycle.LifecycleException;
+
+/**
+ * The HelloWorldBean. Simply prints Hello World and quits the agent afterwards.
+ * 
+ * @author Thomas Konnerth
+ */
+public class HelloBean extends AbstractAgentBean {
+
+  /**
+   * Exection of the HelloWorld Example. Pretty simple.
+   * 
+   * @see de.dailab.jiactng.agentcore.AbstractAgentBean#execute()
+   */
+  public void execute() {
+    // print Hello world
+    System.out.println("Hello World from "+getBeanName());
+while(true) {
+  ;
+}
+    
+//    try {
+//      // stop Agent
+//      thisAgent.getAgentNode().stop();
+//      thisAgent.getAgentNode().cleanup();
+//    } catch (LifecycleException e) {
+//      e.printStackTrace();
+//    }
+
+  }
+
+}

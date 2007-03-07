10f02c4692e9ab860a9fc3003bc5529072bfa12c
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-03-07 17:07:50 +0000

    REMOVED HelloWorld and PingPong moved to new examples module
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@12670 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/test/assembly/standalone/bin/PingPong.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set LOCALCLASSPATH=
-for %%i in ("..\lib\*") do call "lcp.bat" %%i
-
-"%JAVA_HOME%\bin\java" -Dcom.sun.management.jmxremote -cp %LOCALCLASSPATH%;.;..\conf\ de.dailab.jiactng.agentcore.Agent pingPong.xml
-
-pause

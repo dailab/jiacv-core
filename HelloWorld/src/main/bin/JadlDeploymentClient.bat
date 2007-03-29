ab14216cfbdbbb7909560ec9fc8eec732d285a80
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-03-29 13:26:43 +0000

    MOVED jadl deployment client to MamsHelloWorld
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@12952 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- HelloWorld/src/main/bin/JadlDeploymentClient.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set LOCALCLASSPATH=
-for %%i in ("..\lib\*") do call "lcp.bat" %%i
-
-"%JAVA_HOME%\bin\java" -cp %LOCALCLASSPATH%;.;..\conf\ de.dailab.jiactng.jadl.client.JadlDeploymentClient %1 %2
-
-pause

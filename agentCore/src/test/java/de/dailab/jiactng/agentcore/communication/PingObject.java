cfe70a0f3c30ab11127333dde8317707baf8c5b6
joachim <joachim@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-03-29 15:53:25 +0000

    initial commit: test for AMQ based communication
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@12957 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ agentCore/src/test/java/de/dailab/jiactng/agentcore/communication/PingObject.java
@@ -0,0 +1,49 @@
+package de.dailab.jiactng.agentcore.communication;
+
+import de.dailab.jiactng.agentcore.knowledge.IFact;
+
+/**
+ * @author Joachim Fuchs
+ */
+public class PingObject implements IFact {
+    
+    public String string;
+    public boolean receivedFromSpace = false;
+    public boolean receivedFromNetwork = false;
+    
+    /**
+     * Creates a new instance of PingObject
+     *
+     */
+    public PingObject(String string) {
+        
+        this.string = string;
+        
+    }
+    
+    public PingObject(String string, boolean received) {
+        
+        this.string = string;
+        this.receivedFromSpace = received;
+        
+    }
+    
+    public void setReceivedFromSpace() {
+     
+        receivedFromSpace = true;
+        
+    }
+    
+    public void setReceivedFromNetwork() {
+        
+        receivedFromNetwork = true;
+        
+    }
+    
+    public String toString() {
+        
+        return string + " " + receivedFromSpace + " " + receivedFromNetwork;
+        
+    }
+    
+}
---
+++ agentCore/src/test/java/de/dailab/jiactng/agentcore/communication/SpringCommunicationBeanTest.java
@@ -0,0 +1,45 @@
+package de.dailab.jiactng.agentcore.communication;
+
+import de.dailab.jiactng.agentcore.lifecycle.LifecycleException;
+import junit.framework.TestCase;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Joachim Fuchs
+ */
+public class SpringCommunicationBeanTest extends TestCase {
+    
+    ClassPathXmlApplicationContext ctx = null;
+    CommunicationBean instance = null;
+    
+    public SpringCommunicationBeanTest(String testName) {
+        super(testName);
+    }
+    
+    protected void setUp() throws Exception {
+        
+        ctx = new ClassPathXmlApplicationContext(
+                new String [] { "de/dailab/jiactng/agentcore/communication/communicationbeanTest.xml" }
+        );
+        
+        instance = (CommunicationBean)ctx.getBean("communicationBean");
+        
+    }
+    
+    protected void tearDown() throws Exception {
+        
+        if (ctx != null) {
+         
+            ctx.close();
+            
+        }
+        
+    }
+    
+    public void testPlatform() throws Exception {
+        
+        Thread.sleep(30000);
+        
+    }
+    
+}

5e4885ccc2bbf6a73d39c2f0a657cfa11e8990b4
joachim <joachim@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-03 13:01:52 +0000

    Exceptionhandling improved: - doXXX() may throw any kind of Exception now. - e.g. init() throws a lifecycle exception caught from doInit() without a wrapping exception
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@12987 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/lifecycle/AbstractLifecycle.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/lifecycle/AbstractLifecycle.java
@@ -14,20 +14,20 @@ import org.springframework.context.ApplicationContext;
  * @author Joachim Fuchs
  */
 public abstract class AbstractLifecycle extends NotificationBroadcasterSupport implements ILifecycle {
-
+    
     /**
      * The lifecycle handler that is used internally.
      */
     protected DefaultLifecycleHandler lifecycle = null;
-
+    
     public AbstractLifecycle() {
-     
+        
         lifecycle = new DefaultLifecycleHandler(this);
         
     }
     
     public AbstractLifecycle(boolean strict) {
-     
+        
         lifecycle = new DefaultLifecycleHandler(this, strict);
         
     }
@@ -35,155 +35,187 @@ public abstract class AbstractLifecycle extends NotificationBroadcasterSupport i
     /**
      * The number of the next JMX compliant notification.
      */
-    protected long sequenceNumber = 1; 
-
+    protected long sequenceNumber = 1;
+    
     /**
      * Reference to the spring application context
      */
     protected ApplicationContext applicationContext = null;
-
+    
     /**
      * You may override this method to change the lifecycle event propagation behavior.
      */
     public void init() throws LifecycleException {
-
+        
         lifecycle.beforeInit();
-
+        
         try {
-
+            
             doInit();
-
+            
         } catch (Throwable t) {
-
-            throw new LifecycleException("Failed to initialize", t);
-
+            
+            if (t instanceof LifecycleException) {
+                
+                throw (LifecycleException)t;
+                
+            } else {
+                
+                throw new LifecycleException("Failed to initialize", t);
+                
+            }
+            
         }
-
+        
         lifecycle.afterInit();
-
+        
     }
-
+    
     /**
      * You may override this method to change the lifecycle event propagation behavior.
      */
     public void start() throws LifecycleException {
-
+        
         lifecycle.beforeStart();
-
+        
         try {
-
+            
             doStart();
-
+            
         } catch (Throwable t) {
-
-            throw new LifecycleException("Failed to start", t);
-
+            
+            if (t instanceof LifecycleException) {
+                
+                throw (LifecycleException)t;
+                
+            } else {
+                
+                throw new LifecycleException("Failed to start", t);
+                
+            }
+            
         }
-
+        
         lifecycle.afterStart();
-
+        
     }
-
+    
     /**
      * You may override this method to change the lifecycle event propagation behavior.
      */
     public void stop() throws LifecycleException {
-
+        
         lifecycle.beforeStop();
-
+        
         try {
-
+            
             doStop();
-
+            
         } catch (Throwable t) {
-
-            throw new LifecycleException("Failed to stop", t);
-
+            
+            if (t instanceof LifecycleException) {
+                
+                throw (LifecycleException)t;
+                
+            } else {
+                
+                throw new LifecycleException("Failed to stop", t);
+                
+            }
+            
         }
-
+        
         lifecycle.afterStop();
-
+        
     }
-
+    
     /**
      * You may override this method to change the lifecycle event propagation behavior.
      */
     public void cleanup() throws LifecycleException {
-
+        
         lifecycle.beforeCleanup();
-
+        
         try {
-
+            
             doCleanup();
-
+            
         } catch (Throwable t) {
-
-            throw new LifecycleException("Failed to clean up", t);
-
+            
+            if (t instanceof LifecycleException) {
+                
+                throw (LifecycleException)t;
+                
+            } else {
+                
+                throw new LifecycleException("Failed to clean up", t);
+                
+            }
+            
         }
-
+        
         lifecycle.afterCleanup();
-
+        
     }
-
+    
     /**
      * Registers the supplied <code>ILifecycleListener</code>.
      */
     public void addLifecycleListener(ILifecycleListener listener) {
-
+        
         lifecycle.addLifecycleListener(listener);
-
+        
     }
-
+    
     /**
      * Unregisters the supplied <code>ILifecycleListener</code>.
      */
     public void removeLifecycleListener(ILifecycleListener listener) {
-
+        
         lifecycle.removeLifecycleListener(listener);
-
+        
     }
-
+    
     /**
      * Returns the current lifecycle state of this <code>ILifecycle</code>.
      *
      * @return the current lifecycle state
      */
     public LifecycleStates getState() {
-
+        
         return lifecycle.getState();
-
+        
     }
-
+    
     /**
      * @see de.dailab.jiactng.agentcore.AgentMBean#getLifecycleState()
      */
     public String getLifecycleState() {
-  	  System.out.println("Get LifecycleState ...");
-  	  return getState().toString();
+        System.out.println("Get LifecycleState ...");
+        return getState().toString();
     }
-  	
+    
     /**
-     * Uses JMX to send notifications that the attribute "LifecycleState" 
-     * of the managed lifecycle (e.g. agent) has been changed. 
-     * 
+     * Uses JMX to send notifications that the attribute "LifecycleState"
+     * of the managed lifecycle (e.g. agent) has been changed.
+     *
      * @param oldState the old state of the lifecycle
      * @param newState the new state of the lifecycle
      */
     public void stateChanged(LifecycleStates oldState, LifecycleStates newState) {
-    	Notification n = 
-    		new AttributeChangeNotification(this, 
-    				sequenceNumber++, 
-				    System.currentTimeMillis(), 
-				    "LifecycleState changed", 
-				    "LifecycleState", 
-				    "java.lang.String", 
-				    oldState.toString(), 
-				    newState.toString()); 
-
-    	sendNotification(n);
+        Notification n =
+                new AttributeChangeNotification(this,
+                sequenceNumber++,
+                System.currentTimeMillis(),
+                "LifecycleState changed",
+                "LifecycleState",
+                "java.lang.String",
+                oldState.toString(),
+                newState.toString());
+        
+        sendNotification(n);
     }
-
+    
     @Override
     public MBeanNotificationInfo[] getNotificationInfo() {
         String[] types = new String[] {
@@ -192,28 +224,28 @@ public abstract class AbstractLifecycle extends NotificationBroadcasterSupport i
         String name = AttributeChangeNotification.class.getName();
         String description = "An attribute of this MBean has changed";
         MBeanNotificationInfo info =
-            new MBeanNotificationInfo(types, name, description);
+                new MBeanNotificationInfo(types, name, description);
         return new MBeanNotificationInfo[] {info};
     }
-
+    
     /**
      * Put your initialization code here.
      */
-    public abstract void doInit() throws LifecycleException ;
-
+    public abstract void doInit() throws Exception ;
+    
     /**
      * Put your start code here.
      */
-    public abstract void doStart() throws LifecycleException ;
-
+    public abstract void doStart() throws Exception ;
+    
     /**
      * Put your stop code here.
      */
-    public abstract void doStop() throws LifecycleException ;
-
+    public abstract void doStop() throws Exception ;
+    
     /**
      * Put your clean up code here.
      */
-    public abstract void doCleanup() throws LifecycleException ;
-
+    public abstract void doCleanup() throws Exception ;
+    
 }

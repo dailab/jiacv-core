e0f017d4b0e7e621416d0fb789adff51dbe386d0
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-17 14:29:14 +0000

    Kommentar zugefügt/geändert.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13043 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/IJiacSender.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/IJiacSender.java
@@ -2,6 +2,11 @@ package de.dailab.jiactng.agentcore.comm;
 
 import javax.jms.Destination;
 
+/**
+ * Ein allgemeiner Jms Sender für Jiac
+ * @author janko
+ *
+ */
 public interface IJiacSender {
 	/**
 	 * Sendet in default Destination
@@ -10,13 +15,17 @@ public interface IJiacSender {
 	public void send(IJiacMessage message);
 	
 	/**
-	 * Sendet in angegebene Destination
-	 * @param message
-	 * @param destinationName
+	 * Sendet in Destination mit angegebenen Namen
+	 * @param message die jiac-message
+	 * @param destinationName der Name der Destination, daraus wird das DestinationObjekt erzeugt (widerspricht zwar der JMS spezifikation, aber was macht man nich alle saus bequemlichkeit)
 	 */
 	public void send(IJiacMessage message, String destinationName);
 	
-	
+	/**
+	 * Sendet in angegebenen Destination
+	 * @param message die jiac-message
+	 * @param destination die Destination (d.h. Topic oder Queue)
+	 */
 	public void send(IJiacMessage message, Destination destination);
 	
 }

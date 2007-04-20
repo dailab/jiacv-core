2974888fc7a8a480e283648167fa2c93dcf581ca
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-20 09:18:12 +0000

    Unnütze Methoden auskommentiert.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13131 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/JiacMessageFactory.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/JiacMessageFactory.java
@@ -9,17 +9,17 @@ import javax.jms.Destination;
  */
 public class JiacMessageFactory {
 
-	public static IJiacMessage createJiacMessage(String op, IJiacContent payload) {
-		IJiacMessage message = null;
-		message = new JiacMessage(op, payload, EndPointFactory.createEndPoint());
-		return message;
-	}
-
-	public static IJiacMessage createJiacMessage(String op, IJiacContent payload, IEndPoint origin, Destination replyToDestination) {
-		IJiacMessage message = null;
-		message = new JiacMessage(op, payload, EndPointFactory.createEndPoint(), origin, replyToDestination);
-		return message;
-	}
+//	public static IJiacMessage createJiacMessage(String op, IJiacContent payload) {
+//		IJiacMessage message = null;
+//		message = new JiacMessage(op, payload, EndPointFactory.createEndPoint());
+//		return message;
+//	}
+//
+//	public static IJiacMessage createJiacMessage(String op, IJiacContent payload, IEndPoint origin, Destination replyToDestination) {
+//		IJiacMessage message = null;
+//		message = new JiacMessage(op, payload, EndPointFactory.createEndPoint(), origin, replyToDestination);
+//		return message;
+//	}
 
 	public static IJiacMessage createJiacMessage(String op, IJiacContent payload, IEndPoint origin, IEndPoint destEndPoint, Destination replyToDestination) {
 		IJiacMessage message = null;

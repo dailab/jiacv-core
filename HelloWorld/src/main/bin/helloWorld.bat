ac5e557416eee9a983d26cf9d10f3072c5c7bb3e
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-24 16:36:41 +0000

    CHANGED batch files and shell scripts to start an agent node with the SimpleAgentNode class
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@13193 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- HelloWorld/src/main/bin/helloWorld.bat
+++ HelloWorld/src/main/bin/helloWorld.bat
@@ -3,6 +3,6 @@
 set LOCALCLASSPATH=
 for %%i in ("..\lib\*") do call "lcp.bat" %%i
 
-"%JAVA_HOME%\bin\java" -Dcom.sun.management.jmxremote -cp %LOCALCLASSPATH%;.;..\conf\ de.dailab.jiactng.agentcore.Agent helloWorld.xml
+"%JAVA_HOME%\bin\java" -Dcom.sun.management.jmxremote -cp %LOCALCLASSPATH%;.;..\conf\ de.dailab.jiactng.agentcore.SimpleAgentNode helloWorld.xml
 
 pause

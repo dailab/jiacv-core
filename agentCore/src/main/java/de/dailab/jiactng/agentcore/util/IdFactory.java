7941ca7e26f8dcd36ea7cab52c6d34fc31a9ff9c
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-25 12:54:05 +0000

    ADDED platform id generation
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13205 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/util/IdFactory.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/util/IdFactory.java
@@ -1,5 +1,11 @@
 package de.dailab.jiactng.agentcore.util;
 
+/**
+ * Collection of ID creation methods.
+ * TODO concept for id and naming.
+ * 
+ * @author axle
+ */
 public final class IdFactory {
 
 	/**
@@ -8,7 +14,22 @@ public final class IdFactory {
 	private IdFactory() {}
 
 	/**
-	 * Creates a id for an agent node.
+	 * Creates a id for an agent platform. Remember, platform in TNG is a virtual concept
+	 * which acquaints agents with each other regardless of the node they are running on.
+	 * 
+	 * @param hashcode the hashcode of the agent platform object
+	 * @return the id of the agent platform
+	 */
+	public static String createPlatformId(int hashcode) {
+		return new StringBuffer("p:")
+			.append(Long.toHexString(System.currentTimeMillis() + hashcode))
+			.toString();
+	}
+	
+	/**
+	 * Creates a id for an agent node. Remember, agent node coresponds to the Java VM
+	 * the agent is living in.
+	 * 
 	 * @param hashcode the hashcode of the agent node object
 	 * @return the id of the agent node
 	 */
@@ -20,6 +41,7 @@ public final class IdFactory {
 	
 	/**
 	 * Creates a id for an agent.
+	 * 
 	 * @param hashcode the hashcode of the agent object
 	 * @return the id of the agent
 	 */

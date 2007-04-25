d7e4296092361a9a0f684c76a6067d05e321a396
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-25 08:54:48 +0000

    adapted example to doAction-system
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@13197 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- HelloWorld/src/main/java/de/dailab/jiactng/examples/helloWorld/HelloBean.java
+++ HelloWorld/src/main/java/de/dailab/jiactng/examples/helloWorld/HelloBean.java
@@ -3,39 +3,61 @@
  */
 package de.dailab.jiactng.examples.helloWorld;
 
+import java.util.ArrayList;
+
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
-import de.dailab.jiactng.agentcore.lifecycle.LifecycleException;
+import de.dailab.jiactng.agentcore.action.Action;
+import de.dailab.jiactng.agentcore.action.DoAction;
+import de.dailab.jiactng.agentcore.environment.IEffector;
 
 /**
  * The HelloWorldBean. Simply prints Hello World and quits the agent afterwards.
  * 
  * @author Thomas Konnerth
  */
-public class HelloBean extends AbstractAgentBean {
+public class HelloBean extends AbstractAgentBean implements IEffector {
 
-  /**
-   * Exection of the HelloWorld Example. Pretty simple.
-   * 
-   * @see de.dailab.jiactng.agentcore.AbstractAgentBean#execute()
-   */
-  public void execute() {
-    // print Hello world
-    System.out.println("Hello World from "+getBeanName());
-    try {
-		Thread.sleep(1000);
-	} catch (InterruptedException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+	public void doStart() throws Exception {
+		super.doStart();
+		Action a = memory.read(new Action("helloWorld", null, null, null));
+		memory.write(a.createDoAction(new Class[0], this));
 	}
-    
-//    try {
-//      // stop Agent
-//      thisAgent.getAgentNode().stop();
-//      thisAgent.getAgentNode().cleanup();
-//    } catch (LifecycleException e) {
-//      e.printStackTrace();
-//    }
 
-  }
+	/**
+	 * Exection of the HelloWorld Example. Pretty simple.
+	 * 
+	 * @see de.dailab.jiactng.agentcore.AbstractAgentBean#execute()
+	 */
+	public void execute() {
+//		// print Hello world
+//		System.out.println("Hello World from " + getBeanName());
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+	}
 
+	public void doAction(DoAction arg0) {
+		if ("helloWorld".equals(arg0.getThisAction().getName())) {
+			System.out.println("Hello World (" + arg0.getId() + ") from "
+					+ getBeanName());
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Unknown Action: " + arg0);
+		}
+	}
+
+	public ArrayList<Action> getActions() {
+		ArrayList myActions = new ArrayList();
+		myActions
+				.add(new Action("helloWorld", this, new Class[0], new Class[0]));
+		return myActions;
+	}
 }

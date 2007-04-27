e77c39b2387889e8665866704c0afe3ab2aeac0a
schenk <schenk@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-04-27 12:47:48 +0000

    Initial
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13265 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/servicediscovery/IServiceSearchRequest.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package de.dailab.jiactng.agentcore.servicediscovery;
+
+import de.dailab.jiactng.agentcore.knowledge.IFact;
+
+/**
+ * Describes the searchconstraints for a requested service.
+ * 
+ * @author schenk
+ *
+ */
+
+public interface IServiceSearchRequest extends IFact {
+
+}

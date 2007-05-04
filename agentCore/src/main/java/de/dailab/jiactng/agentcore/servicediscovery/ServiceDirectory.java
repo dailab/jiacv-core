0944b330032d1503555939fe67110d590f318067
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-05-04 13:24:04 +0000

    ServiceDirectory umgestrickt
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13392 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/servicediscovery/ServiceDirectory.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/servicediscovery/ServiceDirectory.java
@@ -1,12 +1,20 @@
 package de.dailab.jiactng.agentcore.servicediscovery;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
-import de.dailab.jiactng.agentcore.AbstractAgentBean;
-import de.dailab.jiactng.agentcore.knowledge.Memory;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import de.dailab.jiactng.agentcore.comm.CommBean;
+import de.dailab.jiactng.agentcore.comm.message.IJiacContent;
+import de.dailab.jiactng.agentcore.comm.message.JiacMessage;
+import de.dailab.jiactng.agentcore.comm.message.ObjectContent;
+import de.dailab.jiactng.agentcore.comm.protocol.IServiceProtocol;
+import de.dailab.jiactng.agentcore.lifecycle.AbstractLifecycle;
 
 /**
  * ServiceDirectoryBean soll Beschreibungen aller Services die auf einem AgentNode existieren, verfügbar halten. Dazu
@@ -14,23 +22,102 @@ import de.dailab.jiactng.agentcore.knowledge.Memory;
  * 
  * @author janko
  */
-public class ServiceDirectory extends AbstractAgentBean implements IServiceDirectory {
+public class ServiceDirectory extends AbstractLifecycle implements IServiceDirectory, Runnable {
+	Log log = LogFactory.getLog(getClass());
+	
+	// um auf die topic zu schreiben
+	CommBean _commBean;
+	// zum speichern der Servicebeschreibungen
+	ServiceDirectoryMemory memory;
+	
+	/* um die CommBean zu initialisieren, aus dem Namen wird die Adresse gebildet */
+	String _agentNodeName;
+
+	/* Time in milliseconds between publishment of services */
+	int _publishTimer = 10000;
 
 	public ServiceDirectory() {
 		super();
-		memory = new Memory();
+		memory = new ServiceDirectoryMemory();
 	}
 
 	public void doInit() throws Exception {
-		memory.init();
+		if (_commBean != null) {
+			_commBean.setAgentNodeName(_agentNodeName);
+			_commBean.init();
+		}
 	}
 
-	public void doStart() throws Exception {
-		memory.start();
+	public void doStart() throws Exception {}
+
+	public void doStop() throws Exception {}
+
+	public void doCleanup() throws Exception {
+		if (_commBean != null) {
+			_commBean.cleanup();
+		}
+		memory.doCleanup();
 	}
 
-	public void doStop() throws Exception {
-		memory.stop();
+	public void run() {
+		while (true) {
+			try {
+				Thread.sleep(_publishTimer);
+				execute();
+			} catch (Exception e) {
+				
+			}
+		}
+	}
+
+	/**
+	 * Methode die den Namen liefert unter der der Thread des ServiceDirectory im ThreadPool gespeichert wird
+	 * 
+	 * @param nodeName
+	 * @return ein String als Schlüssel für den ServiceDirectory-Thread
+	 */
+	public String getFutureName(String nodeName) {
+		return "ServiceDirectory:" + nodeName;
+	}
+
+	/**
+	 * periodisch in die topic schreiben
+	 */
+	private void execute() {
+		log.debug("do Publish");
+		doPublishServices();
+	}
+
+	/**
+	 * Macht die eigentlich arbeit
+	 *
+	 */
+	private void doPublishServices() {
+		List<IServiceDescription> serviceList = getAllServices();
+		for (Iterator iter = serviceList.iterator(); iter.hasNext();) {
+			IServiceDescription serviceDesc = (IServiceDescription) iter.next();
+			log.debug("service found");
+			// nur services publishen, deren verfallsdatum in zukunft liegt.
+			Date actualDate = new Date();
+			if (serviceDesc.getExpireDate().after(actualDate)) {
+				long timeDiff = serviceDesc.getExpireDate().getTime()-actualDate.getTime();
+				log.debug("service:"+serviceDesc.getName()+" has "+timeDiff+" ms to live.");
+				_commBean.publish(createJiacMEssage(serviceDesc));
+			}
+		}
+	}
+
+	/**
+	 * bastelt JiacNachrciht zusammen. Sie soll an eine ServicTopic gehen.
+	 * 
+	 * @param serviceDesc die zu verschiekende Servicebeschreibung
+	 * @return die Nachricht die per JMS verschickt wird.
+	 */
+	private JiacMessage createJiacMEssage(IServiceDescription serviceDesc) {
+		IJiacContent content = new ObjectContent(serviceDesc);
+		JiacMessage msg = new JiacMessage(IServiceProtocol.PUB_SERVICE, content, null, _commBean.getAddress(), _commBean
+																						.getCommunicator().getSender().getDefaultReplyDestination());
+		return msg;
 	}
 
 	/**
@@ -119,6 +206,30 @@ public class ServiceDirectory extends AbstractAgentBean implements IServiceDirec
 	 */
 	public List<IServiceDescription> searchService(IServiceSearchRequest request) {
 		return null;
+	}
+
+	public CommBean getCommBean() {
+		return _commBean;
+	}
+
+	public void setCommBean(CommBean commBean) {
+		_commBean = commBean;
+	}
+
+	public int getPublishTimer() {
+		return _publishTimer;
+	}
+
+	public void setPublishTimer(int publishTimer) {
+		_publishTimer = publishTimer;
+	}
+
+	public String getAgentNodeName() {
+		return _agentNodeName;
+	}
+
+	public void setAgentNodeName(String agentNodeName) {
+		_agentNodeName = agentNodeName;
 	}
 
 }
---
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/servicediscovery/ServiceDirectoryMemory.java
@@ -0,0 +1,181 @@
+package de.dailab.jiactng.agentcore.servicediscovery;
+
+import java.util.Iterator;
+import java.util.Set;
+
+import javax.security.auth.DestroyFailedException;
+
+import org.sercho.masp.space.ObjectMatcher;
+import org.sercho.masp.space.ObjectUpdater;
+import org.sercho.masp.space.SimpleObjectSpace;
+import org.sercho.masp.space.event.EventedSpaceWrapper;
+import org.sercho.masp.space.event.EventedTupleSpace;
+import org.sercho.masp.space.event.SpaceObserver;
+import org.sercho.masp.space.event.EventedSpaceWrapper.SpaceDestroyer;
+
+import de.dailab.jiactng.agentcore.knowledge.IFact;
+
+/**
+ * @author Janko Dimitroff
+ * Eigener Memory - der nicht den Lifecycle implementiert. Plain Java class.
+ * Benutzt den TupleSpace.
+ */
+public class ServiceDirectoryMemory {
+
+	private SpaceDestroyer<IFact> destroyer;
+	private EventedTupleSpace<IFact> space;
+
+	public ServiceDirectoryMemory() {
+		doInit();
+	}
+
+	/**
+	 * During initialization the TupleSpace is created.
+	 * 
+	 * @see org.sercho.masp.space.TupleSpace
+	 */
+	public void doInit() {
+		destroyer = EventedSpaceWrapper.getSpaceWithDestroyer(new SimpleObjectSpace<IFact>("FactBase"));
+		space = destroyer.destroybleSpace;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void doStart() {
+	// nothing to do yet
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void doStop() {
+	// nothing to do yet
+	// persistency may go here
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void doCleanup() {
+		try {
+			destroyer.destroy();
+		} catch (DestroyFailedException e) {
+			e.printStackTrace();
+		}
+		space = null;
+		destroyer = null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public String getID() {
+		return space.getID();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ObjectMatcher getMatcher() {
+		return space.getMatcher();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public ObjectUpdater getUpdater() {
+		return space.getUpdater();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> E read(E template) {
+		return space.read(template);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> E read(E template, long timeout) {
+		return space.read(template, timeout);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> Set<E> readAll(E template) {
+		return space.readAll(template);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> Set<E> readAllOfType(Class<E> c) {
+		return space.readAllOfType(c);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> E remove(E template) {
+		return space.remove(template);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> E remove(E template, long timeout) {
+		return space.remove(template, timeout);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> Set<E> removeAll(E template) {
+		return space.removeAll(template);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public <E extends IFact> boolean update(E template, E pattern) {
+		return space.update(template, pattern);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void write(IFact fact) {
+		space.write(fact);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Iterator<IFact> iterator() {
+		return space.iterator();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void attach(SpaceObserver<? super IFact> observer) {
+		space.attach(observer);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void attach(SpaceObserver<? super IFact> observer, IFact template) {
+		space.attach(observer, template);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public void detach(SpaceObserver<? super IFact> observer) {
+		space.detach(observer);
+	}
+}

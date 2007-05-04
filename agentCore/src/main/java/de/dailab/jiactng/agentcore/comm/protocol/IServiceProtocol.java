d4620f082f646cd6e8a8c202c2015294d9f5dbe9
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-05-04 13:22:37 +0000

    Interface für zukünftiges ServiceProtocol
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13390 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/protocol/IServiceProtocol.java
@@ -0,0 +1,8 @@
+package de.dailab.jiactng.agentcore.comm.protocol;
+
+public interface IServiceProtocol extends IProtocolHandler {
+	
+	/* Nachricht veröffentliche einen Service */
+	public static final String PUB_SERVICE = "PUB_SERVICE";
+	
+}

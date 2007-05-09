191f2bd303e2c9000188f4134fd03d9b91270300
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-05-09 12:58:04 +0000

    formatierung
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13459 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/knowledge/IFact.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/knowledge/IFact.java
@@ -3,10 +3,8 @@ package de.dailab.jiactng.agentcore.knowledge;
 import java.io.Serializable;
 
 /**
- * The interface is though to be the super interface
- * of all knowledge add to and removed from the
- * memory.
+ * The interface is though to be the super interface of all knowledge add to and removed from the memory.
  */
-public interface IFact extends Serializable{
+public interface IFact extends Serializable {
 
 }

54f51ff77a4956a2c264de4019d843a3fff70752
axle <axle@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-05-23 14:48:10 +0000

    ADDED javadoc comments, tsetse wie kann man sowas denn vergessen
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@13585 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/ontology/OtherAgentDescription.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/ontology/OtherAgentDescription.java
@@ -2,14 +2,30 @@ package de.dailab.jiactng.agentcore.ontology;
 
 import de.dailab.jiactng.agentcore.comm.message.IEndPoint;
 
+/**
+ * Holds information about other agents.
+ * @author axle
+ *
+ */
 public class OtherAgentDescription extends AgentDescription {
 
 	private static final long serialVersionUID = -6296106391008839370L;
 
+	/**
+	 * Creates a new description of another agent.
+	 * @param aid the agent id
+	 * @param name the name of the other agent
+	 * @param state the state of the other agent
+	 * @param endpoint the address under which the other agent can be reached
+	 */
 	public OtherAgentDescription(String aid, String name, String state, IEndPoint endpoint) {
 		super(aid, name, state, endpoint);
 	}
 
+	/**
+	 * Creates the description of another agent from a given agent description.
+	 * @param descr the agent description to create a description of an other agent
+	 */
 	public OtherAgentDescription(AgentDescription descr) {
 		super(descr.getAid(), descr.getName(), descr.getState(), descr.getEndpoint());
 	}

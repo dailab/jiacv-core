e3de07fb0783b59338b389ad1d906c8f13bc90bf
marcel <marcel@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-08-02 14:49:59 +0000

    CHANGE: removed SimpleLocalCommBean
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@14126 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/Version.java
+++ agentCore/src/main/java/de/dailab/jiactng/Version.java
@@ -4,7 +4,7 @@ public final class Version
 {	
 	private final static String NAME  		= "agentCore";	
 	private final static String NUMBER  	= "0.0.1-SNAPSHOT";
-	private final static String TIMESTAMP	= "01.08.2007 16:53:38";
+	private final static String TIMESTAMP	= "02.08.2007 15:01:07";
 	
 	/**
 	 *	@return jiac revision name 

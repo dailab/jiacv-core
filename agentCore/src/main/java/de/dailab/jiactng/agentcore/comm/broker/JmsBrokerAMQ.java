77a983106570b5ff5e3d1c656ee93c6b5d0156b6
marcel <marcel@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-08-21 15:49:29 +0000

    CHANGE: JMS broker is now started in the initialise method according to the needs of the CommunicationBean
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@14224 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/broker/JmsBrokerAMQ.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/broker/JmsBrokerAMQ.java
@@ -18,139 +18,132 @@ import de.dailab.jiactng.agentcore.lifecycle.LifecycleException;
  * @author Joachim Fuchs, Janko Dimitroff
  */
 public class JmsBrokerAMQ extends AbstractLifecycle {
-	/** The logger we use, if it is not set by DI, we create our own */
-	protected Log log = LogFactory.getLog(getClass());
+    /** The logger we use, if it is not set by DI, we create our own */
+    protected Log log = LogFactory.getLog(getClass());
 
-	public BrokerValues values = null;
+    public BrokerValues values = null;
 
-	/** The embedded broker we use if no other broker is running on our host machine */
-	protected BrokerService broker = null;
+    /** The embedded broker we use if no other broker is running on our host machine */
+    protected BrokerService broker = null;
 
-	/** The connector we use to connect to the broker */
-	protected TransportConnector connector = null;
+    /** The connector we use to connect to the broker */
+    protected TransportConnector connector = null;
 
-	public void setLog(Log log) {
-		this.log = log;
-	}
+    public void setLog(Log log) {
+        this.log = log;
+    }
 
-	// ---------------------- Lifecycle
-	public void doInit() throws Exception {
-		if (log == null){
-			System.err.println("No log set for Embedded Broker -> init aborted!");
-			return;
-		}
-		
-		if (log.isInfoEnabled()){
-			log.info("initializing embedded broker");
-		}
-		
-		if (values._url == null){
-			values.setUrlFromPortAndProtocol();
-		}
-		
-		broker = new BrokerService();
-		broker.setBrokerName(values.getName());
-		broker.setUseJmx(false); // values.isJmx()
-		broker.setPersistent(values.isPersistent());
-		try {
-		
-			connector = broker.addConnector(values.getUrl());
-			connector.setDiscoveryUri(new URI(getDiscoveryUri(values.getDiscoveryMethod(), values.getDiscoveryAddress())));
-			broker.addNetworkConnector(new URI(getDiscoveryUri(values.getDiscoveryMethod(), values.getDiscoveryAddress())));
-			connector.getDiscoveryAgent().setBrokerName(values.getName());
-		} catch (BindException be) {
-			log.error(be.getCause().toString());
-			log.warn("No Broker will be started");
-			broker = null;
-		}
-		if(log.isInfoEnabled()) {
-			log.info("embedded broker initialized. url = " + values.getUrl());
-		}
-	}
+    // ---------------------- Lifecycle
+    public void doInit() throws Exception {
+        log.debug("initializing embedded broker");
+        
+        if (values._url == null){
+            values.setUrlFromPortAndProtocol();
+        }
+        
+        broker = new BrokerService();
+        broker.setBrokerName(values.getName());
+        broker.setUseJmx(false); // values.isJmx()
+        broker.setPersistent(values.isPersistent());
+        try {
+        
+            connector = broker.addConnector(values.getUrl());
+            connector.setDiscoveryUri(new URI(getDiscoveryUri(values.getDiscoveryMethod(), values.getDiscoveryAddress())));
+            broker.addNetworkConnector(new URI(getDiscoveryUri(values.getDiscoveryMethod(), values.getDiscoveryAddress())));
+            connector.getDiscoveryAgent().setBrokerName(values.getName());
+        } catch (BindException be) {
+            log.error(be.getCause().toString());
+            log.warn("No Broker will be started");
+            broker = null;
+        }
+        log.debug("embedded broker initialized. url = " + values.getUrl());
+        
+        
+        // start broker
+        if (broker != null) {
+            log.debug("starting broker");
+            connector.start();
+            broker.start();
+            log.debug("broker started");
+        } else {
+            log.warn("no broker found to start");
+        }
+    }
 
-	public void doStart() throws Exception {
-		
-		// start broker
-		if (broker != null) {
-			if (log.isInfoEnabled()){
-				log.info("EmbeddedBroker is starting...");
-			}
-			connector.start();
-			broker.start();
-			if(log.isInfoEnabled()){
-				log.info("broker started");
-			}
-		} else {
-			if (log.isErrorEnabled()){
-				log.error("no broker found to start!");
-			}
-		}
-	}
+    public void doStart() throws Exception {
+//      // start broker
+//      if (broker != null) {
+//          log.debug("starting broker");
+//          connector.start();
+//          broker.start();
+//          log.debug("broker started");
+//      } else {
+//          log.warn("no broker found to start");
+//      }
+    }
 
-	public void doStop() throws Exception {
-		// stop broker
-		if (broker != null) {
-			if (log.isInfoEnabled()){
-				log.info("stopping embedded broker");
-			}
-			connector.stop();
-			broker.stop();
-			
-			if(log.isInfoEnabled()){
-				log.debug("embedded broker stopped");
-			}
-		} else {
-			if (log.isWarnEnabled()){
-				log.warn("No Broker found to Stop");
-			}
-		}
-	}
+    public void doStop() throws Exception {
+//      // stop broker
+//      if (broker != null) {
+//          log.debug("stopping broker");
+//          connector.stop();
+//          broker.stop();
+//          log.debug("broker stopped");
+//      } else {
+//          log.warn("No Broker found to stop");
+//      }
+    }
 
-	public void doCleanup() throws Exception {
-		if (broker != null){
-			if (log.isInfoEnabled()){
-				log.info("cleaning up broker");
-				log.info("broker cleaned up");
-			}
-			
-		} else {
-			if (log.isWarnEnabled()){
-				log.warn("No Broker to cleanup");
-			}
-		}
-	}
+    public void doCleanup() throws Exception {
+        // stop broker
+        if (broker != null) {
+            log.debug("stopping broker");
+            connector.stop();
+            broker.stop();
+            log.debug("broker stopped");
+        } else {
+            log.warn("No Broker found to stop");
+        }
+        
+        if (broker != null){
+            log.debug("cleaning up broker");
+            log.debug("broker cleaned up");
+        } else {
+            log.warn("No Broker to cleanup");
+        }
+    }
 
-	/**
-	 * dummy for tests only
-	 */
-	public void springStart() throws Exception {
-		init();
-		start();
-	}
+    /**
+     * dummy for tests only
+     */
+    public void springStart() throws Exception {
+        init();
+        start();
+    }
 
-	/**
-	 * dummy for tests only
-	 */
-	public void springStop() throws Exception {
-		stop();
-		try {
-			cleanup();
-		} catch (LifecycleException le) {
-			log.error(le.getCause());
-		}
-	}
+    /**
+     * dummy for tests only
+     */
+    public void springStop() throws Exception {
+        stop();
+        try {
+            cleanup();
+        } catch (LifecycleException le) {
+            log.error(le.getCause());
+        }
+    }
 
-	protected String getDiscoveryUri(String discoveryMethod, String discoveryAddress) {
-		return discoveryMethod + "://" + discoveryAddress;
-	}
+    protected String getDiscoveryUri(String discoveryMethod, String discoveryAddress) {
+        return discoveryMethod + "://" + discoveryAddress;
+    }
 
-	// ------------------------ Setter/Getter
-	public BrokerValues getValues() {
-		return values;
-	}
+    // ------------------------ Setter/Getter
+    public BrokerValues getValues() {
+        return values;
+    }
 
-	public void setValues(BrokerValues values) {
-		this.values = values;
-	}
+    public void setValues(BrokerValues values) {
+        this.values = values;
+    }
 
 }

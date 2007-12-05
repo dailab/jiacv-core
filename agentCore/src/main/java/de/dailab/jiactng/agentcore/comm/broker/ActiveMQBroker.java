1f29f5bea4ffa335ad02b96f1a20a72ed01a9863
marcel <marcel@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-12-05 14:55:20 +0000

    CHANGE: die konfigurationen angepasst verbessert CHANGE: jetzt duerfte erstmal wieder kein test gehen
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@15723 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/broker/ActiveMQBroker.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/broker/ActiveMQBroker.java
@@ -1,12 +1,8 @@
 package de.dailab.jiactng.agentcore.comm.broker;
 
 import java.net.URI;
-import java.util.HashSet;
 import java.util.Set;
 
-import javax.jms.ConnectionFactory;
-
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.apache.activemq.broker.TransportConnector;
 import org.apache.activemq.broker.jmx.ManagementContext;
@@ -19,17 +15,11 @@ import de.dailab.jiactng.agentcore.AbstractAgentNodeBean;
  * 
  */
 public class ActiveMQBroker extends AbstractAgentNodeBean {
-    public static final String PROTOCOL_IP_SEPARATOR = "://";
-
-    public static final char IP_PORT_SEPARATOR = ':';
-
     /** The embedded broker we use if no other broker is running on our host machine */
-    private BrokerService _broker = new BrokerService();
-    private Set<String> _connectors = new HashSet<String>();
-    private String _discoveryMethod = null;
-    private String _discoveryAddress = null;
+    private String _brokerName= null;
+    private BrokerService _broker= null;
+    private Set<ActiveMQTransportConnector> _connectors= null;
     private boolean _persistent = false;
-    private boolean _jmx = true;
 
     public ActiveMQBroker() {}
 
@@ -37,33 +27,33 @@ public class ActiveMQBroker extends AbstractAgentNodeBean {
     public void doInit() throws Exception {
         log.debug("initializing embedded broker");
         _broker = new BrokerService();
-        _broker.setBrokerName(getBeanName());
-        _broker.setUseJmx(_jmx);
+        _broker.setBrokerName(getBrokerName());
+        
+        if(agentNode.isManagementEnabled()) {
+            _broker.setUseJmx(true);
+            ManagementContext context = new ManagementContext();
+            context.setJmxDomainName("de.dailab.jiactng");
+            context.setCreateConnector(false);
+            _broker.setManagementContext(context);
+        }
+        
         _broker.setPersistent(_persistent);
 
-        ManagementContext context = new ManagementContext();
-        context.setJmxDomainName("de.dailab.jiactng");
-        context.setCreateConnector(false);
-        _broker.setManagementContext(context);
-
         try {
-            for (String url : _connectors) {
-                log.debug("embedded broker initializing url = " + url);
-                TransportConnector connector = _broker.addConnector(url);
-                if (_discoveryMethod != null && _discoveryAddress != null) {
-                    URI uri = new URI(_discoveryMethod + "://" + _discoveryAddress);
-            		log.debug("adding discoveryAgent on address: " + uri);
-                    connector.setDiscoveryUri(uri);
-                    connector.getDiscoveryAgent().setBrokerName(_broker.getBrokerName());
-                    _broker.addNetworkConnector(uri);
-                    connector.start();
+            for (ActiveMQTransportConnector amtc : _connectors) {
+                log.debug("embedded broker initializing url = " + amtc.toString());
+                TransportConnector connector = _broker.addConnector(new URI(amtc.getTransportURI()));
+                if (amtc.getDiscoveryURI() != null) {
+//                    URI uri = new URI(amtc.getDiscoveryURI());
+                    connector.setDiscoveryUri(new URI(amtc.getDiscoveryURI()));
+//                    connector.getDiscoveryAgent().setBrokerName(_broker.getBrokerName());
+//                    _broker.addNetworkConnector(uri);
                 }
             }
 
         } catch (Exception e) {
             log.error(e.toString());
         }
-        log.debug("embedded broker initialized: Persistent=" + _persistent + ", UseJMX=" + _jmx);
 
         _broker.start();
         log.debug("started broker");
@@ -74,31 +64,52 @@ public class ActiveMQBroker extends AbstractAgentNodeBean {
         log.debug("stopping broker");
     }
 
-    public void setJmx(boolean jmx) {
-        _jmx = jmx;
-    }
-
+    /**
+     * Indicates whether messages should be stored in a data base or not.
+     * ActiveMQ uses Derby to store message.
+     * You must include this dependency explicitly in your project as it is
+     * not associated with agentCore.
+     * 
+     * @param persistent    <code>true</code> to store messages and <code>false</code> otherwise.
+     */
     public void setPersistent(boolean persistent) {
         _persistent = persistent;
     }
 
-    public void setConnectors(Set<String> urlList) {
-        _connectors = urlList;
+    /**
+     * Setter for the set of connectors.
+     * Connectors are entry points to the broker that accept remote connections.
+     * By default, every broker has a logical vm-connector which permits the
+     * inner-vm-message exchange.
+     * 
+     * <p>
+     * This method should be called before this bean is initialised!
+     * </p>
+     * 
+     * @param connectors    the set of connectors
+     */
+    public void setConnectors(Set<ActiveMQTransportConnector> connectors) {
+        _connectors = connectors;
     }
 
-    public void setDiscoveryMethod(String discoveryMethod) {
-        _discoveryMethod = discoveryMethod;
-    }
-
-    public void setDiscoveryAddress(String discoveryAddress) {
-        _discoveryAddress = discoveryAddress;
+    /**
+     * Setter for the broker name. The name is only changed when this node bean is not
+     * yet initialised. Otherwise the call of this method is ignored.
+     * <p>
+     * You have to ensure, that the broker name is unique. If the broker name is not set
+     * the name of this bean will be taken instead!
+     * </p>
+     * 
+     * @param brokerName    the new name for the broker or <code>null</code> to default it
+     *                      to the bean name. 
+     */
+    public void setBrokerName(String brokerName) {
+        if(_broker == null) {
+            _brokerName= brokerName;
+        }
     }
     
-    // FACTORY METHODS FOR CONNECTOR CREATION
-    public ConnectionFactory createConnectionFactory() {
-        ActiveMQConnectionFactory factory=  new ActiveMQConnectionFactory("vm://" + getBeanName());
-//        factory.setUseAsyncSend(true);
-//        factory.setDispatchAsync(true);
-        return factory;
+    public String getBrokerName() {
+        return _brokerName == null ? getBeanName() : _brokerName;
     }
 }
---
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/broker/ActiveMQTransportConnector.java
@@ -0,0 +1,53 @@
+/*
+ * $Id$ 
+ */
+package de.dailab.jiactng.agentcore.comm.broker;
+
+
+/**
+ * @author Marcel Patzlaff
+ * @version $Revision:$
+ */
+public class ActiveMQTransportConnector {
+    private String _transportURI= null;
+    private String _discoveryURI= null;
+    
+    /**
+     * The URI where remote connections are accepted. Examples are:
+     * <pre>
+     *   tcp://localhost:61616
+     *   stomp://dai076.dai-lab.de:61613
+     * </pre>
+     * 
+     * @param transportURI
+     */
+    public void setTransportURI(String transportURI) {
+        _transportURI= transportURI;
+    }
+    
+    /**
+     * The URI for the discovery agent of this connector. For example:
+     * <pre>
+     *   multicast://239.255.2.45:5555
+     * </pre>
+     * 
+     * @param discoveryURI  the discovery URI or <code>null</code>
+     *                      if discovery should be deactivated
+     */
+    public void setDiscoveryURI(String discoveryURI) {
+        _discoveryURI= discoveryURI;
+    }
+    
+    public String getTransportURI() {
+        return _transportURI;
+    }
+    
+    public String getDiscoveryURI() {
+        return _discoveryURI;
+    }
+
+    @Override
+    public String toString() {
+        return "transportURI: " + getTransportURI() + "; discoverURI: " + getDiscoveryURI();
+    }
+}

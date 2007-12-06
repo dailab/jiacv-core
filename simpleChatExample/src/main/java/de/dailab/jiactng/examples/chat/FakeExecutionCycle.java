17a62d2986a32a94c99344974082eda4d4a85694
marcel <marcel@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2007-12-06 09:38:52 +0000

    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@15732 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ simpleChatExample/src/main/java/de/dailab/jiactng/examples/chat/FakeExecutionCycle.java
@@ -0,0 +1,22 @@
+/*
+ * $Id$ 
+ */
+package de.dailab.jiactng.examples.chat;
+
+import de.dailab.jiactng.agentcore.AbstractAgentBean;
+import de.dailab.jiactng.agentcore.IAgent;
+import de.dailab.jiactng.agentcore.IExecutionCycle;
+
+/**
+ * @author Marcel Patzlaff
+ * @version $Revision:$
+ */
+public class FakeExecutionCycle extends AbstractAgentBean implements IExecutionCycle {
+    public void setAgent(IAgent agent) {
+        setThisAgent(agent);
+    }
+
+    public void run() {
+
+    }
+}

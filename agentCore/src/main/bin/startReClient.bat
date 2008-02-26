97704e371f69459f36e6da86ba885b0d29d3fa39
janko <janko@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-02-26 16:02:25 +0000

    weg damit
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@16507 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/bin/startReClient.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set LOCALCLASSPATH=
-for %%i in ("..\lib\*") do call "lcp.bat" %%i
-
-"%JAVA_HOME%\bin\java" -cp %LOCALCLASSPATH%;. de.dailab.jiactng.sercho.rulesengine.client.RulesRmiClient localhost 20000 http://192.168.2.2:8080/shea/services/Notification
-
-pause

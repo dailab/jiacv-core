1a0731db1e2af262e2b85ac1269fb6007afcd060
keiser <keiser@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-03-11 14:40:50 +0000

    New: JiacMessage provides a description based on JMX open types. This allows to handle message information remotely without having the application-specific classes used in the message content.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@16741 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/message/JiacMessage.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/message/JiacMessage.java
@@ -1,14 +1,23 @@
 package de.dailab.jiactng.agentcore.comm.message;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
+import javax.management.openmbean.CompositeDataSupport;
+import javax.management.openmbean.CompositeType;
+import javax.management.openmbean.OpenDataException;
+import javax.management.openmbean.OpenType;
+import javax.management.openmbean.SimpleType;
+
 import de.dailab.jiactng.agentcore.comm.CommunicationAddressFactory;
 import de.dailab.jiactng.agentcore.comm.ICommunicationAddress;
 import de.dailab.jiactng.agentcore.knowledge.IFact;
+import de.dailab.jiactng.agentcore.management.jmx.JmxDescriptionSupport;
 
 /**
  * The implementation of {@link IJiacMessage}.
@@ -16,7 +25,11 @@ import de.dailab.jiactng.agentcore.knowledge.IFact;
  * @author Marcel Patzlaff
  */
 @SuppressWarnings("serial")
-public class JiacMessage implements IJiacMessage {
+public class JiacMessage implements IJiacMessage, JmxDescriptionSupport {
+
+	public static final String ITEMNAME_HEADERS = "headers";
+	public static final String ITEMNAME_PAYLOAD = "payload";
+
     private IFact _payload;
 
     private Map<String, String> _headers;
@@ -186,4 +199,96 @@ public class JiacMessage implements IJiacMessage {
     public void setSender(ICommunicationAddress sender) {
         setHeader(Header.SENDER, sender != null ? sender.toString() : null);
     }
+
+    /**
+     * Gets the type of JIAC message descriptions based on JMX open types.
+     * @return A composite type containing headers and payload.
+     * @throws OpenDataException if an error occurs during the creation of the type.
+     * @see javax.management.openmbean.CompositeType
+     */
+    public OpenType<?> getDescriptionType() throws OpenDataException {
+    	ArrayList<String> itemNames = new ArrayList<String>();
+    	ArrayList<OpenType<?>> itemTypes = new ArrayList<OpenType<?>>();
+    	
+    	// create open type of message payload
+    	if (_payload != null) {
+    		itemNames.add(ITEMNAME_PAYLOAD);
+    		if (_payload instanceof JmxDescriptionSupport) {
+    			itemTypes.add(((JmxDescriptionSupport)_payload).getDescriptionType());
+    		} else {
+    			itemTypes.add(SimpleType.STRING);
+    		}
+    	}
+    	
+    	// create open type of message headers
+    	if (_headers != null) {
+    		itemNames.add(ITEMNAME_HEADERS);
+    		String[] headerNames = getHeaderKeys().toArray(new String[0]);
+    		int headerSize = headerNames.length;
+    		OpenType<?>[] headerTypes = new OpenType<?>[headerSize];
+    		for (int i=0; i<headerSize; i++) {
+    			// each header value is of type String
+    			headerTypes[i] = SimpleType.STRING;
+    		}
+    		itemTypes.add(new CompositeType(
+    				_headers.getClass().getName(), 
+    				"headers of a JIAC-TNG message",
+    				headerNames,
+    				headerNames,
+    				headerTypes));
+    	}
+    	
+    	// use names of message items as their description
+    	ArrayList<String> itemDescriptions = itemNames;
+    	
+    	// transform list to array
+    	int compositeSize = itemTypes.size();
+		OpenType<?>[] itemTypesArray = new OpenType<?>[compositeSize];
+    	for (int i=0; i<compositeSize; i++) {
+    		itemTypesArray[i] = itemTypes.get(i);
+    	}
+    	
+    	// create and return open type of a JIAC message
+    	return new CompositeType(
+    			this.getClass().getName(), 
+    			"standard JIAC-TNG message", 
+    			itemNames.toArray(new String[0]),
+    			itemDescriptions.toArray(new String[0]),
+    			itemTypesArray);
+    }
+
+    /**
+     * Gets the description of this JIAC message based on JMX open types.
+     * @return Composite data containing headers and payload.
+     * @throws OpenDataException if an error occurs during the creation of the data.
+     * @see javax.management.openmbean.CompositeData
+     */
+	public Object getDescription() throws OpenDataException {
+		Map<String,Object> items = new HashMap<String,Object>();
+		CompositeType type = (CompositeType) getDescriptionType();
+
+		// create open data of message payload
+		if (_payload != null) {
+    		if (_payload instanceof JmxDescriptionSupport) {
+    			items.put(ITEMNAME_PAYLOAD, ((JmxDescriptionSupport)_payload).getDescription());
+    		} else {
+    			items.put(ITEMNAME_PAYLOAD, _payload.toString());
+    		}
+		}
+
+		// create open data of message headers
+    	if (_headers != null) {
+    		String[] headerNames = getHeaderKeys().toArray(new String[0]);
+    		int headerSize = headerNames.length;
+    		Object[] headerValues = new Object[headerSize];
+    		for (int i=0; i<headerSize; i++) {
+    			headerValues[i] = getHeader(headerNames[i]);
+    		}
+    		items.put(ITEMNAME_HEADERS, new CompositeDataSupport(
+    				(CompositeType) type.getType(ITEMNAME_HEADERS),
+    				headerNames, headerValues));
+    	}
+    	
+		return new CompositeDataSupport(type, items);
+	}
 }

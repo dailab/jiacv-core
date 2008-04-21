f614fd2860b5decbcba053ec9f2ed7d115fed26d
keiser <keiser@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-04-21 15:43:41 +0000

    Uses new features of TNG to simplify example.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@17427 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- HelloWorld/src/main/java/de/dailab/jiactng/examples/helloWorld/HelloBean.java
+++ HelloWorld/src/main/java/de/dailab/jiactng/examples/helloWorld/HelloBean.java
@@ -3,61 +3,24 @@
  */
 package de.dailab.jiactng.examples.helloWorld;
 
-import java.util.ArrayList;
-
-import de.dailab.jiactng.agentcore.AbstractAgentBean;
+import de.dailab.jiactng.agentcore.action.AbstractMethodExposingBean;
 import de.dailab.jiactng.agentcore.action.Action;
-import de.dailab.jiactng.agentcore.action.DoAction;
-import de.dailab.jiactng.agentcore.environment.IEffector;
 
 /**
- * The HelloWorldBean. Simply prints Hello World and quits the agent afterwards.
+ * The HelloWorldBean. Simply prints Hello World.
  * 
  * @author Thomas Konnerth
  */
-public class HelloBean extends AbstractAgentBean implements IEffector {
+public class HelloBean extends AbstractMethodExposingBean {
 
 	public void doStart() throws Exception {
 		super.doStart();
-		Action a = memory.read(new Action("helloWorld", null, new Class[0], null));
+		Action a = memory.read(new Action("helloWorld"));
 		memory.write(a.createDoAction(new Class[0], null));
 	}
 
-	/**
-	 * Exection of the HelloWorld Example. Pretty simple.
-	 * 
-	 * @see de.dailab.jiactng.agentcore.AbstractAgentBean#execute()
-	 */
-	public void execute() {
-//		// print Hello world
-//		System.out.println("Hello World from " + getBeanName());
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-	}
-
-	public void doAction(DoAction arg0) {
-		if ("helloWorld".equals(arg0.getAction().getName())) {
-			log.info("Hello World (" + arg0.getSessionId() + ") from "
-					+ getBeanName());
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		} else {
-			log.error("Unknown Action: " + arg0);
-		}
-	}
-
-	public ArrayList<Action> getActions() {
-		ArrayList<Action> myActions = new ArrayList<Action>();
-		myActions
-				.add(new Action("helloWorld", this, new Class[0], new Class[0]));
-		return myActions;
+	@Expose(name = "helloWorld")
+	public void helloWorld() {
+		log.info("Hello World");
 	}
 }

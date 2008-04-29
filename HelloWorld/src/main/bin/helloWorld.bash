5b05d3eeea9353b12ab6f6734d15d78c716f6509
kaisers <kaisers@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-04-29 12:36:21 +0000

    Replacing shell script for standalone start with working bash version.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@17568 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ HelloWorld/src/main/bin/helloWorld.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo $JAVA_HOME
+
+# set classpath
+DYNCLASSPATH=''
+for i in $( ls ../lib ); do
+        DYNCLASSPATH=$DYNCLASSPATH':'../lib/$i
+done
+# echo "Dynamic Classpath: "$DYNCLASSPATH
+
+# construct classpath
+export CLASSPATH='.:'$CLASSPATH':'$DYNCLASSPATH
+
+# Java Options
+JAVA_OPTIONS='-Dcom.sun.management.jmxremote -cp '$CLASSPATH':../conf/'
+# echo 'Java Options: '$JAVA_OPTIONS
+
+# Main Class
+MAIN_CLASS='de.dailab.jiactng.agentcore.SimpleAgentNode'
+
+# Main Class Parameters
+MAIN_CLASS_PARAMETERS='../conf/helloWorld.xml'
+
+
+#
+# Java execution
+#
+echo $JAVA_HOME/bin/java $JAVA_OPTIONS $MAIN_CLASS $MAIN_CLASS_PARAMETERS
+
+$JAVA_HOME/bin/java $JAVA_OPTIONS $MAIN_CLASS $MAIN_CLASS_PARAMETERS
+

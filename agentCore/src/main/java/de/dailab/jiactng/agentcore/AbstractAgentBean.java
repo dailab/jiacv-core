3036c8bfc18227b2ba98d228b4640bce5f2f9223
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-08-06 13:12:37 +0000

    NEW: added new name for logging configuration to avoid conflicts, and did some cleanup to utitlity-methods in AbstractAgentBean
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@19604 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/AbstractAgentBean.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/AbstractAgentBean.java
@@ -26,322 +26,314 @@ import de.dailab.jiactng.agentcore.ontology.IAgentDescription;
  * 
  * @author Thomas Konnerth
  */
-public abstract class AbstractAgentBean extends AbstractLifecycle implements
-		IAgentBean, AbstractAgentBeanMBean {
+public abstract class AbstractAgentBean extends AbstractLifecycle implements IAgentBean, AbstractAgentBeanMBean {
 
-	/**
-	 * Interval by which the execute()-method of the bean is called. If
-	 * negative, the execute-method is never called.
-	 */
-	private int executeInterval = -1;
+  /**
+   * Interval by which the execute()-method of the bean is called. If negative,
+   * the execute-method is never called.
+   */
+  private int       executeInterval   = -1;
 
-	/**
-	 * Used by the execution cycle to determine the next execution time when
-	 * <code>executeInterval</code> is greater than 0.
-	 */
-	private long nextExecutionTime = 0;
+  /**
+   * Used by the execution cycle to determine the next execution time when
+   * <code>executeInterval</code> is greater than 0.
+   */
+  private long      nextExecutionTime = 0;
 
-	/** The manager of the agent node */
-	protected Manager _manager = null;
+  /** The manager of the agent node */
+  protected Manager _manager          = null;
 
-	/**
-	 * Creates an agent bean that uses lifecycle support in loose mode
-	 */
-	public AbstractAgentBean() {
-		super();
-	}
+  /**
+   * Creates an agent bean that uses lifecycle support in loose mode
+   */
+  public AbstractAgentBean() {
+    super();
+  }
 
-//	/**
-//	 * Creates an agent bean that may use lifecycle support in strict mode. This
-//	 * means, that the lifecycle graph is enforced.
-//	 * 
-//	 * @param strict
-//	 *            Flag, that determines, whether the lifecylce of this agentbean
-//	 *            should run in strict mode or not.
-//	 * @see ILifecycle
-//	 */
-//	public AbstractAgentBean(boolean strict) {
-//		super(strict);
-//	}
+  // /**
+  // * Creates an agent bean that may use lifecycle support in strict mode. This
+  // * means, that the lifecycle graph is enforced.
+  // *
+  // * @param strict
+  // * Flag, that determines, whether the lifecylce of this agentbean
+  // * should run in strict mode or not.
+  // * @see ILifecycle
+  // */
+  // public AbstractAgentBean(boolean strict) {
+  // super(strict);
+  // }
 
-	/**
-	 * Reference to the agent that holds this bean.
-	 */
-	protected IAgent thisAgent = null;
+  /**
+   * Reference to the agent that holds this bean.
+   */
+  protected IAgent  thisAgent = null;
 
-	/**
-	 * Reference to the memory of the agent that holds this bean.
-	 */
-	protected IMemory memory = null;
+  /**
+   * Reference to the memory of the agent that holds this bean.
+   */
+  protected IMemory memory    = null;
 
-	/**
-	 * The name this bean. Note that this is the unqualified name which is
-	 * assigned by Spring.
-	 */
-	protected String beanName = null;
+  /**
+   * The name this bean. Note that this is the unqualified name which is
+   * assigned by Spring.
+   */
+  protected String  beanName  = null;
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public final void setThisAgent(IAgent agent) {
-		// update management
-		if (isManagementEnabled()) {
-			Manager manager = _manager;
-			disableManagement();
-			this.thisAgent = agent;
-			enableManagement(manager);
-		} else {
-			this.thisAgent = agent;
-		}
+  /**
+   * {@inheritDoc}
+   */
+  public final void setThisAgent(IAgent agent) {
+    // update management
+    if (isManagementEnabled()) {
+      Manager manager = _manager;
+      disableManagement();
+      this.thisAgent = agent;
+      enableManagement(manager);
+    } else {
+      this.thisAgent = agent;
+    }
 
-		// update logger
-		setLog(thisAgent.getLog(this));
-	}
+    // update logger
+    setLog(thisAgent.getLog(this));
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public final void setMemory(IMemory mem) {
-		this.memory = mem;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public final void setMemory(IMemory mem) {
+    this.memory = mem;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public final void setBeanName(String name) {
-		// update management
-		if (isManagementEnabled()) {
-			Manager manager = _manager;
-			disableManagement();
-			this.beanName = name;
-			enableManagement(manager);
-		} else {
-			this.beanName = name;
-		}
+  /**
+   * {@inheritDoc}
+   */
+  public final void setBeanName(String name) {
+    // update management
+    if (isManagementEnabled()) {
+      Manager manager = _manager;
+      disableManagement();
+      this.beanName = name;
+      enableManagement(manager);
+    } else {
+      this.beanName = name;
+    }
 
-		// update logger
-		if (thisAgent != null) {
-			setLog(thisAgent.getLog(this));
-		}
-	}
+    // update logger
+    if (thisAgent != null) {
+      setLog(thisAgent.getLog(this));
+    }
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public final String getBeanName() {
-		return beanName;
-		// return new StringBuffer(thisAgent.getAgentName()).append(".").append(
-		// beanName).toString();
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public final String getBeanName() {
+    return beanName;
+    // return new StringBuffer(thisAgent.getAgentName()).append(".").append(
+    // beanName).toString();
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void doInit() throws Exception {
-		if (log == null) {
-			setLog(thisAgent.getLog(this));
-		}
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void doInit() throws Exception {
+    if (log == null) {
+      setLog(thisAgent.getLog(this));
+    }
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void doStart() throws Exception {
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void doStart() throws Exception {
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void doStop() throws Exception {
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void doStop() throws Exception {
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void doCleanup() throws Exception {
-	}
-	
-	/**
-	 * is only meant to be called through SimpleExecutionCycle during Timeoutmanagment
-	 * 
-	 * @param doAction the action execution to be canceled
-	 * @return the result of the canceled action (always <code>null</code> if this method is not overwritten)
-	 */
-	public ActionResult cancelAction(DoAction doAction){
-		return null;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public void doCleanup() throws Exception {
+  }
 
-	/**
-	 * Registers the agent bean and all its resources for management
-	 * 
-	 * @param manager
-	 *            the manager responsible for this agentbean.
-	 */
-	public void enableManagement(Manager manager) {
-		// do nothing if management already enabled
-		if (isManagementEnabled()) {
-			return;
-		}
+  /**
+   * is only meant to be called through SimpleExecutionCycle during
+   * Timeoutmanagment
+   * 
+   * @param doAction
+   *          the action execution to be canceled
+   * @return the result of the canceled action (always <code>null</code> if
+   *         this method is not overwritten)
+   */
+  public ActionResult cancelAction(DoAction doAction) {
+    return null;
+  }
 
-		// register agent bean for management
-		try {
-			manager.registerAgentBean(this, thisAgent);
-		} catch (Exception e) {
-			System.err.println("WARNING: Unable to register agent bean "
-					+ beanName + " of agent " + thisAgent.getAgentName()
-					+ " of agent node " + thisAgent.getAgentNode().getName()
-					+ " as JMX resource.");
-			System.err.println(e.getMessage());
-		}
+  /**
+   * Registers the agent bean and all its resources for management
+   * 
+   * @param manager
+   *          the manager responsible for this agentbean.
+   */
+  public void enableManagement(Manager manager) {
+    // do nothing if management already enabled
+    if (isManagementEnabled()) {
+      return;
+    }
 
-		_manager = manager;
-	}
+    // register agent bean for management
+    try {
+      manager.registerAgentBean(this, thisAgent);
+    } catch (Exception e) {
+      System.err.println("WARNING: Unable to register agent bean " + beanName + " of agent " + thisAgent.getAgentName()
+          + " of agent node " + thisAgent.getAgentNode().getName() + " as JMX resource.");
+      System.err.println(e.getMessage());
+    }
 
-	/**
-	 * Deregisters the agent bean and all its resources from management.
-	 */
-	public void disableManagement() {
-		// do nothing if management already disabled
-		if (!isManagementEnabled()) {
-			return;
-		}
+    _manager = manager;
+  }
 
-		// deregister agent bean from management
-		try {
-			_manager.unregisterAgentBean(this, thisAgent);
-		} catch (Exception e) {
-			System.err.println("WARNING: Unable to deregister agent bean "
-					+ beanName + " of agent " + thisAgent.getAgentName()
-					+ " of agent node " + thisAgent.getAgentNode().getName()
-					+ " as JMX resource.");
-			System.err.println(e.getMessage());
-		}
+  /**
+   * Deregisters the agent bean and all its resources from management.
+   */
+  public void disableManagement() {
+    // do nothing if management already disabled
+    if (!isManagementEnabled()) {
+      return;
+    }
 
-		_manager = null;
-	}
+    // deregister agent bean from management
+    try {
+      _manager.unregisterAgentBean(this, thisAgent);
+    } catch (Exception e) {
+      System.err.println("WARNING: Unable to deregister agent bean " + beanName + " of agent "
+          + thisAgent.getAgentName() + " of agent node " + thisAgent.getAgentNode().getName() + " as JMX resource.");
+      System.err.println(e.getMessage());
+    }
 
-	/**
-	 * Checks wether the management of this object is enabled or not.
-	 * 
-	 * @return true if the management is enabled, otherwise false
-	 */
-	public boolean isManagementEnabled() {
-		return _manager != null;
-	}
+    _manager = null;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public int getExecuteInterval() {
-		return executeInterval;
-	}
+  /**
+   * Checks wether the management of this object is enabled or not.
+   * 
+   * @return true if the management is enabled, otherwise false
+   */
+  public boolean isManagementEnabled() {
+    return _manager != null;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setExecuteInterval(int executeInterval) {
-		try {
-			if (executeInterval <= 0) {
-				nextExecutionTime = 0;
-				return;
-			}
-			// execute Interval > 0, schedule/reschedule bean
-			if (nextExecutionTime > 0) {
-				nextExecutionTime = nextExecutionTime - this.executeInterval
-						+ executeInterval;
-			} else {
-				nextExecutionTime = System.currentTimeMillis()
-						+ executeInterval;
-			}
-		} finally {
-			this.executeInterval = executeInterval;
-		}
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public int getExecuteInterval() {
+    return executeInterval;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public long getNextExecutionTime() {
-		return nextExecutionTime;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public void setExecuteInterval(int executeInterval) {
+    try {
+      if (executeInterval <= 0) {
+        nextExecutionTime = 0;
+        return;
+      }
+      // execute Interval > 0, schedule/reschedule bean
+      if (nextExecutionTime > 0) {
+        nextExecutionTime = nextExecutionTime - this.executeInterval + executeInterval;
+      } else {
+        nextExecutionTime = System.currentTimeMillis() + executeInterval;
+      }
+    } finally {
+      this.executeInterval = executeInterval;
+    }
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void setNextExecutionTime(long nextExecutionTime) {
-		this.nextExecutionTime = nextExecutionTime;
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public long getNextExecutionTime() {
+    return nextExecutionTime;
+  }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void execute() {
+  /**
+   * {@inheritDoc}
+   */
+  public void setNextExecutionTime(long nextExecutionTime) {
+    this.nextExecutionTime = nextExecutionTime;
+  }
 
-	}
+  /**
+   * {@inheritDoc}
+   */
+  public void execute() {
 
-	/**
-	 * {@inheritDoc}
-	 */
-	public void handleLifecycleException(LifecycleException e, @SuppressWarnings("unused") LifecycleStates state) {
-		throw new RuntimeException(e);
-	}
-	
-	protected String invoke(Action a, Serializable[] inputParams) {
-		return invoke(a, inputParams, null);
-	}
+  }
 
-	protected String invoke(Action a, Serializable[] inputParams,
-			ResultReceiver receiver) {
-		DoAction doAct = a.createDoAction(inputParams, receiver);
-		memory.write(doAct);
-		return doAct.getSessionId();
-	}
-	
-  protected String invoke(Action a, Session parent, Serializable[] inputParams,
-      ResultReceiver receiver) {
+  /**
+   * {@inheritDoc}
+   */
+  public void handleLifecycleException(LifecycleException e, @SuppressWarnings("unused")
+  LifecycleStates state) {
+    throw new RuntimeException(e);
+  }
+
+  protected String invoke(Action a, Serializable[] inputParams) {
+    return invoke(a, inputParams, null);
+  }
+
+  protected String invoke(Action a, Serializable[] inputParams, ResultReceiver receiver) {
+    DoAction doAct = a.createDoAction(inputParams, receiver);
+    memory.write(doAct);
+    return doAct.getSessionId();
+  }
+
+  protected String invoke(Action a, Session parent, Serializable[] inputParams, ResultReceiver receiver) {
     DoAction doAct = a.createDoAction(parent, inputParams, receiver);
     memory.write(doAct);
     return doAct.getSessionId();
-  }	
-	
-//	protected ActionResult syncInvoke(Action a, Object[] inputParams) {
-//		DoAction doAct = a.createDoAction(inputParams, null);
-//		return ((Agent)thisAgent).syncInvoke(doAct);
-//	}
-	
-	protected void returnResult(DoAction origin, Serializable[] results) {
-		ActionResult res = ((Action) origin.getAction()).createActionResult(
-				origin, results);
-		memory.write(res);
-	}
-	
-	protected void returnFailure(DoAction origin, Serializable failure) {
-		ActionResult res = new ActionResult(origin,failure);
-		memory.write(res);
-	}
-	
-	protected Action retrieveAction(String actionName) {
-		Action retAct = memory.read(new Action(actionName));
-		if (retAct == null) {
-			log.warn("Could not find \'" + actionName
-					+ "\'.");
-		}
-		return retAct;
-	}
-	
-	protected Action retrieveAction(String actionName, IAgentDescription provider) {
-		Action template = new Action(actionName);
-		template.setProviderDescription(provider);
-		
-		Action retAct = memory.read(template);
-		if (retAct == null) {
-			log.warn("Could not find \'" + actionName
-					+ "\'.");
-		}
-		return retAct;
-	}
+  }
+
+  // protected ActionResult syncInvoke(Action a, Object[] inputParams) {
+  // DoAction doAct = a.createDoAction(inputParams, null);
+  // return ((Agent)thisAgent).syncInvoke(doAct);
+  // }
+
+  protected void returnResult(DoAction origin, Serializable[] results) {
+    ActionResult res = ((Action) origin.getAction()).createActionResult(origin, results);
+    memory.write(res);
+  }
+
+  protected void returnFailure(DoAction origin, Serializable failure) {
+    ActionResult res = new ActionResult(origin, failure);
+    memory.write(res);
+  }
+
+  protected Action retrieveAction(String actionName) {
+    Action retAct = memory.read(new Action(actionName));
+    if (retAct == null) {
+      log.warn("Could not find \'" + actionName + "\'.");
+    }
+    return retAct;
+  }
+
+  protected Action retrieveAction(String actionName, IAgentDescription provider) {
+    Action template = new Action(actionName);
+    template.setProviderDescription(provider);
+
+    Action retAct = memory.read(template);
+    if (retAct == null) {
+      log.warn("Could not find \'" + actionName + "\'.");
+    }
+    return retAct;
+  }
 }

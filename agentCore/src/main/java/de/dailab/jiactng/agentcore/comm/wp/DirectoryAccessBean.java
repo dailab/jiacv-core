8c95225f5c51cbd7d83d35193f4d16634920d818
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-11-10 14:52:19 +0000

    NEW: added originalUser, originalProvider and originalService to session
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@20921 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/wp/DirectoryAccessBean.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/comm/wp/DirectoryAccessBean.java
@@ -970,6 +970,10 @@ public class DirectoryAccessBean extends AbstractAgentBean implements IEffector,
 						} else {
 							// so after finding it let's create the actual DoAction for our agent here
 							remoteDoAction = foundAction.createDoAction(params, this);
+							// add session related information. TODO: maybe think of a better session transfer
+							remoteDoAction.getSession().setOriginalService(doAction.getSession().getOriginalService());
+							remoteDoAction.getSession().setOriginalUser(doAction.getSession().getOriginalUser());
+							remoteDoAction.getSession().setOriginalProvider(doAction.getSession().getOriginalProvider());
 							synchronized (openSessionsFromClients) {
 								openSessionsFromClients.put(remoteDoAction.getSessionId(), new SessionData(doAction, message.getSender()));	
 							}

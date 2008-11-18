829df155d5ee21a4ddecca7bc16a31f69c8bef1a
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-11-18 14:10:37 +0000

    CHANGE: added failure-handling for invokeRemote
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@21038 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/AbstractAgentBean.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/AbstractAgentBean.java
@@ -24,28 +24,26 @@ import de.dailab.jiactng.agentcore.management.Manager;
 import de.dailab.jiactng.agentcore.ontology.IAgentDescription;
 
 /**
- * Abstract superclass of all agentbeans. This includes core-components as well
- * as agentbeans. The class handles basic references (such as to memory and the
- * agent) and defines the methods that are necessary for (lifecycle-)management.
+ * Abstract superclass of all agentbeans. This includes core-components as well as agentbeans. The class handles basic
+ * references (such as to memory and the agent) and defines the methods that are necessary for (lifecycle-)management.
  * 
  * @author Thomas Konnerth
  */
 public abstract class AbstractAgentBean extends AbstractLifecycle implements IAgentBean, AbstractAgentBeanMBean {
 
   /**
-   * Interval by which the execute()-method of the bean is called. If negative,
-   * the execute-method is never called.
+   * Interval by which the execute()-method of the bean is called. If negative, the execute-method is never called.
    */
-  private int       executeInterval   = -1;
+  private int                       executeInterval   = -1;
 
   /**
-   * Used by the execution cycle to determine the next execution time when
-   * <code>executeInterval</code> is greater than 0.
+   * Used by the execution cycle to determine the next execution time when <code>executeInterval</code> is greater
+   * than 0.
    */
-  private long      nextExecutionTime = 0;
+  private long                      nextExecutionTime = 0;
 
-  private HashMap<String, DoAction> searchToInvokeMap = new HashMap<String,DoAction>();
-  
+  private HashMap<String, DoAction> searchToInvokeMap = new HashMap<String, DoAction>();
+
   /**
    * Creates an agent bean that uses lifecycle support in loose mode
    */
@@ -77,8 +75,7 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
   protected IMemory memory    = null;
 
   /**
-   * The name this bean. Note that this is the unqualified name which is
-   * assigned by Spring.
+   * The name this bean. Note that this is the unqualified name which is assigned by Spring.
    */
   protected String  beanName  = null;
 
@@ -168,13 +165,11 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
   }
 
   /**
-   * is only meant to be called through SimpleExecutionCycle during
-   * Timeoutmanagment
+   * is only meant to be called through SimpleExecutionCycle during Timeoutmanagment
    * 
    * @param doAction
    *          the action execution to be canceled
-   * @return the result of the canceled action (always <code>null</code> if
-   *         this method is not overwritten)
+   * @return the result of the canceled action (always <code>null</code> if this method is not overwritten)
    */
   public ActionResult cancelAction(DoAction doAction) {
     return null;
@@ -282,33 +277,38 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
   }
 
   /**
-   * Invokes an action and waits for its result. 
-   * NOTE: This method MUST NOT be used with a blocking execution cycle (e.g. SimpleExecutionCycle).
+   * Invokes an action and waits for its result. NOTE: This method MUST NOT be used with a blocking execution cycle
+   * (e.g. SimpleExecutionCycle).
    * 
-   * @param a The action to be invoked.
-   * @param inputParams The values for the input parameters.
+   * @param a
+   *          The action to be invoked.
+   * @param inputParams
+   *          The values for the input parameters.
    * @return The result of the action.
    */
   protected ActionResult invokeAndWaitForResult(Action a, Serializable[] inputParams) {
-	// invoke action
-	ActionResultListener listener = new ActionResultListener();
-	invoke(a, inputParams, listener);
+    // invoke action
+    ActionResultListener listener = new ActionResultListener();
+    invoke(a, inputParams, listener);
 
-	// wait for result
-	synchronized (listener) {
-		try {
-			listener.wait();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	return listener.getResult();
+    // wait for result
+    synchronized (listener) {
+      try {
+        listener.wait();
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return listener.getResult();
   }
 
   /**
    * Invokes an action asynchronously without handling the result.
-   * @param a the action to be invoked.
-   * @param inputParams the input parameters used for the action invocation.
+   * 
+   * @param a
+   *          the action to be invoked.
+   * @param inputParams
+   *          the input parameters used for the action invocation.
    * @return the session id of the action invocation.
    */
   protected String invoke(Action a, Serializable[] inputParams) {
@@ -317,9 +317,13 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
 
   /**
    * Invokes an action asynchronously with handling the result by a given receiver.
-   * @param a the action to be invoked.
-   * @param inputParams the input parameters used for the action invocation.
-   * @param receiver the receiver to be informed about the results.
+   * 
+   * @param a
+   *          the action to be invoked.
+   * @param inputParams
+   *          the input parameters used for the action invocation.
+   * @param receiver
+   *          the receiver to be informed about the results.
    * @return the session id of the action invocation.
    */
   protected String invoke(Action a, Serializable[] inputParams, ResultReceiver receiver) {
@@ -330,10 +334,15 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
 
   /**
    * Invokes an action asynchronously as part of a parent session with handling the result by a given receiver.
-   * @param a the action to be invoked.
-   * @param parent the session of the parent action.
-   * @param inputParams the input parameters used for the action invocation.
-   * @param receiver the receiver to be informed about the results.
+   * 
+   * @param a
+   *          the action to be invoked.
+   * @param parent
+   *          the session of the parent action.
+   * @param inputParams
+   *          the input parameters used for the action invocation.
+   * @param receiver
+   *          the receiver to be informed about the results.
    * @return the session id of the action invocation.
    */
   protected String invoke(Action a, Session parent, Serializable[] inputParams, ResultReceiver receiver) {
@@ -348,9 +357,12 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
   // }
 
   /**
-   * Sends back the results of an action. 
-   * @param origin the invocation of the action.
-   * @param results the results of the action.
+   * Sends back the results of an action.
+   * 
+   * @param origin
+   *          the invocation of the action.
+   * @param results
+   *          the results of the action.
    */
   protected void returnResult(DoAction origin, Serializable[] results) {
     ActionResult res = ((Action) origin.getAction()).createActionResult(origin, results);
@@ -358,9 +370,12 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
   }
 
   /**
-   * Sends back the failure information of an action. 
-   * @param origin the invocation of the action.
-   * @param failure the failure information of the action.
+   * Sends back the failure information of an action.
+   * 
+   * @param origin
+   *          the invocation of the action.
+   * @param failure
+   *          the failure information of the action.
    */
   protected void returnFailure(DoAction origin, Serializable failure) {
     ActionResult res = new ActionResult(origin, failure);
@@ -369,7 +384,9 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
 
   /**
    * Retrieves an action with a given name from the local memory.
-   * @param actionName the name of the action to be searched for.
+   * 
+   * @param actionName
+   *          the name of the action to be searched for.
    * @return a found action matching the given name or null if no such action was found.
    */
   protected Action retrieveAction(String actionName) {
@@ -382,8 +399,11 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
 
   /**
    * Retrieves an action with a given name and provider from the local memory.
-   * @param actionName the name of the action to be searched for.
-   * @param provider the provider of the action to be search for.
+   * 
+   * @param actionName
+   *          the name of the action to be searched for.
+   * @param provider
+   *          the provider of the action to be search for.
    * @return a found action matching the given name and provider or null if no such action was found.
    */
   protected Action retrieveAction(String actionName, IAgentDescription provider) {
@@ -399,134 +419,139 @@ public abstract class AbstractAgentBean extends AbstractLifecycle implements IAg
 
   /**
    * Invokes an action for the search of actions in the service directory.
-   * @param template The template for searching actions.
-   * @param isGlobal <code>true</code> for a global search and <code>false</code> for an agent node internal search.
-   * @param timeout The maximum duration of the global search. This value will be ignored by local search.
-   * @param receiver the receiver to be informed about the found actions.
+   * 
+   * @param template
+   *          The template for searching actions.
+   * @param isGlobal
+   *          <code>true</code> for a global search and <code>false</code> for an agent node internal search.
+   * @param timeout
+   *          The maximum duration of the global search. This value will be ignored by local search.
+   * @param receiver
+   *          the receiver to be informed about the found actions.
    * @return the session id of the action invocation.
-   * @throws RuntimeException if the agent has no directory access bean.
+   * @throws RuntimeException
+   *           if the agent has no directory access bean.
    */
   protected String invokeActionSearch(Action template, boolean isGlobal, long timeout, ResultReceiver receiver) {
-	Action requestSearchAction = memory.read(new Action(DirectoryAccessBean.ACTION_REQUEST_SEARCH));
-	if (requestSearchAction == null) {
-		throw new RuntimeException("DirectoryAccessBean not available");
-	}
-	Serializable[] params = {template, new Boolean(isGlobal), new Long(timeout)}; 
-	return invoke(requestSearchAction, params, receiver);
+    Action requestSearchAction = memory.read(new Action(DirectoryAccessBean.ACTION_REQUEST_SEARCH));
+    if (requestSearchAction == null) {
+      throw new RuntimeException("DirectoryAccessBean not available");
+    }
+    Serializable[] params = { template, new Boolean(isGlobal), new Long(timeout) };
+    return invoke(requestSearchAction, params, receiver);
   }
 
   /**
-   * Gets from the service directory a list of actions which match the given template. 
-   * NOTE: This method MUST NOT be used with a blocking execution cycle (e.g. SimpleExecutionCycle).
+   * Gets from the service directory a list of actions which match the given template. NOTE: This method MUST NOT be
+   * used with a blocking execution cycle (e.g. SimpleExecutionCycle).
    * 
-   * @param template The template for matching actions.
-   * @param isGlobal <code>true</code> for a global search and <code>false</code> for an agent node internal search.
-   * @param timeout The maximum duration of the global search. This value will be ignored by local search.
-   * @throws RuntimeException if the search failed or was not possible.
+   * @param template
+   *          The template for matching actions.
+   * @param isGlobal
+   *          <code>true</code> for a global search and <code>false</code> for an agent node internal search.
+   * @param timeout
+   *          The maximum duration of the global search. This value will be ignored by local search.
+   * @throws RuntimeException
+   *           if the search failed or was not possible.
    * @return The list of found actions.
    */
   protected List<Action> retrieveActionsFromDirectory(Action template, boolean isGlobal, long timeout) {
-	// get search action
-	Action requestSearchAction = memory.read(new Action(DirectoryAccessBean.ACTION_REQUEST_SEARCH));
-	if (requestSearchAction == null) {
-		throw new RuntimeException("DirectoryAccessBean not available");
-	}
+    // get search action
+    Action requestSearchAction = memory.read(new Action(DirectoryAccessBean.ACTION_REQUEST_SEARCH));
+    if (requestSearchAction == null) {
+      throw new RuntimeException("DirectoryAccessBean not available");
+    }
 
-	// invoke search action
-	Serializable[] params = {template, Boolean.valueOf(isGlobal), Long.valueOf(timeout)}; 
-	ActionResult result = invokeAndWaitForResult(requestSearchAction, params);
-	if (result == null) {
-		throw new RuntimeException("Got no result");
-	}
+    // invoke search action
+    Serializable[] params = { template, Boolean.valueOf(isGlobal), Long.valueOf(timeout) };
+    ActionResult result = invokeAndWaitForResult(requestSearchAction, params);
+    if (result == null) {
+      throw new RuntimeException("Got no result");
+    }
 
-	// get result
-	Serializable[] results = result.getResults();
-	if (results != null) {
-		if (results.length == 1) {
-			try {
-				return (List<Action>) results[0];
-			} catch (ClassCastException e) {
-				throw new RuntimeException("Got wrong type of results");
-			}
-		} else {
-			throw new RuntimeException("Got wrong number of results");
-		}
-	}
+    // get result
+    Serializable[] results = result.getResults();
+    if (results != null) {
+      if (results.length == 1) {
+        try {
+          return (List<Action>) results[0];
+        } catch (ClassCastException e) {
+          throw new RuntimeException("Got wrong type of results");
+        }
+      } else {
+        throw new RuntimeException("Got wrong number of results");
+      }
+    }
 
-	// search failed
-	Serializable failure = result.getFailure();
-	if (failure != null) {
-		if (failure instanceof Throwable) {
-			throw new RuntimeException((Throwable) failure);
-		} else if (failure instanceof String) {
-			throw new RuntimeException((String) failure);
-		} else {
-			throw new RuntimeException("Search for actions failed");
-		}
-	}
+    // search failed
+    Serializable failure = result.getFailure();
+    if (failure != null) {
+      if (failure instanceof Throwable) {
+        throw new RuntimeException((Throwable) failure);
+      } else if (failure instanceof String) {
+        throw new RuntimeException((String) failure);
+      } else {
+        throw new RuntimeException("Search for actions failed");
+      }
+    }
 
-	throw new RuntimeException("Got neigther results nor failure");
+    throw new RuntimeException("Got neigther results nor failure");
   }
-  
+
   protected String invokeRemote(Action template, Session parent, Serializable[] inputParams, ResultReceiver receiver) {
-    if(parent == null) {
+    if (parent == null) {
       parent = new Session(receiver);
     }
-    
+
     Action a = memory.read(template);
-    if(a != null) {
-      log.warn("Found in memory... invoking");
+    if (a != null) {
       return invoke(a, parent, inputParams, receiver);
     } else {
-      DoAction doAct = new DoAction(parent,template, receiver, inputParams);
-//      searchToInvokeMap.put(parent.getSessionId(),doAct);
-//      invokeActionSearch(template,true,1000,this);
+      DoAction doAct = new DoAction(parent, template, receiver, inputParams);
       Action requestSearchAction = memory.read(new Action(DirectoryAccessBean.ACTION_REQUEST_SEARCH));
       if (requestSearchAction == null) {
         throw new RuntimeException("DirectoryAccessBean not available");
       }
-      Serializable[] params = {template, new Boolean(true), new Long(5000)};
-      log.warn("invoking requestSerach with: "+template+" / "+parent.getSessionId() );
-//      invoke(requestSearchAction,parent,params, new SearchResultHandler());
-      
+      Serializable[] params = { template, new Boolean(true), new Long(1000) };
+
       DoAction readAction = requestSearchAction.createDoAction(params, new SearchResultHandler());
-      searchToInvokeMap.put(readAction.getSessionId(),doAct);
-      memory.write(readAction);      
-      
-      
+      searchToInvokeMap.put(readAction.getSessionId(), doAct);
+      memory.write(readAction);
+
       return parent.getSessionId();
     }
   }
 
-  
   private class SearchResultHandler implements ResultReceiver {
 
-    /* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see de.dailab.jiactng.agentcore.environment.ResultReceiver#receiveResult(de.dailab.jiactng.agentcore.action.ActionResult)
      */
     @Override
     public void receiveResult(ActionResult result) {
       String readSession = result.getSessionId();
-      log.warn("Receveid result for: "+readSession);
       DoAction doAct = searchToInvokeMap.get(readSession);
-      if((doAct != null) &&  (result.getFailure() == null)) {
+      if ((doAct != null) && (result.getFailure() == null)) {
         Serializable[] res = result.getResults();
-        List<Action> actList = (List<Action>)res[0];
-        for(Action a : actList) {
+        List<Action> actList = (List<Action>) res[0];
+        for (Action a : actList) {
           memory.write(a);
         }
-        if(actList.size()>0) {
+        if (actList.size() > 0) {
           Action myAct = actList.get(0);
           doAct.setAction(myAct);
           memory.write(doAct);
         } else {
-          log.error("No Actions found for: "+readSession);
+          log.warn("No Actions found for: " + doAct.getAction());
+          returnFailure(doAct, "Could not find Action: " + doAct.getAction());
         }
-      } else {
-        log.error("Encountered problem with: "+readSession);
+      } else if ((doAct != null) && (result.getFailure() != null)) {
+        returnFailure(doAct, result.getFailure());
       }
     }
-    
+
   }
-  
+
 }

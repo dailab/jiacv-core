db719dc34a14b2b8f8660c624033acc1002295cf
marcel <marcel@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2008-12-18 14:33:04 +0000

    CHANGE: copied code from activemq and removed all warnings
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@21482 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- gateway/src/main/java/de/dailab/jiac/net/discovery/multicast/MulticastDiscoveryAgent.java
+++ gateway/src/main/java/de/dailab/jiac/net/discovery/multicast/MulticastDiscoveryAgent.java
@@ -4,79 +4,392 @@
 package de.dailab.jiac.net.discovery.multicast;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.SocketAddress;
+import java.net.SocketTimeoutException;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.apache.activemq.command.DiscoveryEvent;
 import org.apache.activemq.transport.discovery.DiscoveryAgent;
 import org.apache.activemq.transport.discovery.DiscoveryListener;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
+ * This class is mainly a clone of
+ * {@link org.apache.activemq.transport.discovery.multicast.MulticastDiscoveryAgent}!
+ * 
  * @author Marcel Patzlaff
  * @version $Revision:$
  */
-public class MulticastDiscoveryAgent implements DiscoveryAgent {
+public class MulticastDiscoveryAgent implements DiscoveryAgent,Runnable{
+    private static final Log log=LogFactory.getLog(MulticastDiscoveryAgent.class);
+    public static final String DEFAULT_DISCOVERY_URI_STRING="multicast://239.255.2.3:6155";
+    private static final String TYPE_SUFFIX="ActiveMQ-4.";
+    private static final String ALIVE="alive.";
+    private static final String DEAD="dead.";
+    private static final String DELIMITER = "%";
+    private static final int BUFF_SIZE=8192;
+    private static final int DEFAULT_IDLE_TIME=500;
+    private static final int HEARTBEAT_MISS_BEFORE_DEATH=4;
+    
+    protected DiscoveryListener discoveryListener;
+    
+    private int _timeToLive=1;
+    private boolean _loopBackMode=false;
+    private Map<String, AtomicLong> _services=new ConcurrentHashMap<String, AtomicLong>();
+    private Map<String, String> _brokers = new ConcurrentHashMap<String, String>();
+    private String _group="default";
+    private String _brokerName;
+    private URI _discoveryURI;
+    private InetAddress _inetAddress;
+    private SocketAddress _sockAddress;
+    private String _selfService;
+    private MulticastSocket _mcast;
+    private Thread _runner;
+    private long _keepAliveInterval=DEFAULT_IDLE_TIME;
+    private long _lastAdvertizeTime=0;
+    private AtomicBoolean _started=new AtomicBoolean(false);
+    private boolean _reportAdvertizeFailed=true;
+    
+    private final Executor executor = new ThreadPoolExecutor(1, 1, 30, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(), new ThreadFactory() {
+        public Thread newThread(Runnable runable) {
+            Thread t = new Thread(runable, "Multicast Discovery Agent Notifier");
+            t.setDaemon(true);
+            return t;
+        }            
+    });
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#getGroup()
+    /**
+     * Set the discovery listener
+     * 
+     * @param listener
      */
-    public String getGroup() {
-        // TODO Auto-generated method stub
-        return null;
+    public void setDiscoveryListener(DiscoveryListener listener){
+        this.discoveryListener=listener;
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#registerService(java.lang.String)
+    /**
+     * register a service
      */
-    public void registerService(String name) throws IOException {
-        // TODO Auto-generated method stub
-
+    public void registerService(String name) {//throws IOException{
+        this._selfService=name;
+        if (_started.get()){
+            doAdvertizeSelf();
+        }
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#serviceFailed(org.apache.activemq.command.DiscoveryEvent)
+    /**
+     * Get the group used for discovery
+     * 
+     * @return the group
      */
-    public void serviceFailed(DiscoveryEvent event) throws IOException {
-        // TODO Auto-generated method stub
-
+    public String getGroup(){
+        return _group;
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#setBrokerName(java.lang.String)
+    /**
+     * Set the group for discovery
+     * 
+     * @param group
      */
-    public void setBrokerName(String brokerName) {
-        // TODO Auto-generated method stub
-
+    public void setGroup(String group){
+        this._group=group;
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#setDiscoveryListener(org.apache.activemq.transport.discovery.DiscoveryListener)
+    /**
+     * @return Returns the brokerName.
      */
-    public void setDiscoveryListener(DiscoveryListener listener) {
-        // TODO Auto-generated method stub
-
+    public String getBrokerName(){
+        return _brokerName;
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgent#setGroup(java.lang.String)
+    /**
+     * @param brokerName The brokerName to set.
      */
-    public void setGroup(String group) {
-        // TODO Auto-generated method stub
-
+    public void setBrokerName(String brokerName){
+        if (brokerName != null){
+            brokerName = brokerName.replace('.','-');
+            brokerName = brokerName.replace(':','-');
+            brokerName = brokerName.replace('%','-');
+        this._brokerName=brokerName;
+        }
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.Service#start()
+    /**
+     * @return Returns the loopBackMode.
      */
-    public void start() throws Exception {
-        // TODO Auto-generated method stub
-
+    public boolean isLoopBackMode(){
+        return _loopBackMode;
     }
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.Service#stop()
+    /**
+     * @param loopBackMode
+     *            The loopBackMode to set.
      */
-    public void stop() throws Exception {
-        // TODO Auto-generated method stub
-
+    public void setLoopBackMode(boolean loopBackMode){
+        this._loopBackMode=loopBackMode;
     }
 
+    /**
+     * @return Returns the timeToLive.
+     */
+    public int getTimeToLive(){
+        return _timeToLive;
+    }
+
+    /**
+     * @param timeToLive
+     *            The timeToLive to set.
+     */
+    public void setTimeToLive(int timeToLive){
+        this._timeToLive=timeToLive;
+    }
+
+    /**
+     * @return the discoveryURI
+     */
+    public URI getDiscoveryURI(){
+        return _discoveryURI;
+    }
+
+    /**
+     * Set the discoveryURI
+     * 
+     * @param discoveryURI
+     */
+    public void setDiscoveryURI(URI discoveryURI){
+        this._discoveryURI=discoveryURI;
+    }
+
+    public long getKeepAliveInterval(){
+        return _keepAliveInterval;
+    }
+
+    public void setKeepAliveInterval(long keepAliveInterval){
+        this._keepAliveInterval=keepAliveInterval;
+    }
+
+    /**
+     * start the discovery agent
+     * 
+     * @throws Exception
+     */
+    public void start() throws Exception{
+        if(_started.compareAndSet(false,true)){
+            if(_group==null|| _group.length()==0){
+                throw new IOException("You must specify a group to discover");
+            }
+            if (_brokerName == null || _brokerName.length()==0){
+                log.warn("brokerName not set");
+            }
+            String type=getType();
+            if(!type.endsWith(".")){
+                log.warn("The type '"+type+"' should end with '.' to be a valid Discovery type");
+                type+=".";
+            }
+            if(_discoveryURI==null){
+                _discoveryURI=new URI(DEFAULT_DISCOVERY_URI_STRING);
+            }
+            this._inetAddress=InetAddress.getByName(_discoveryURI.getHost());
+            this._sockAddress=new InetSocketAddress(this._inetAddress,_discoveryURI.getPort());
+            _mcast=new MulticastSocket(_discoveryURI.getPort());
+            _mcast.setLoopbackMode(_loopBackMode);
+            _mcast.setTimeToLive(getTimeToLive());
+            _mcast.joinGroup(_inetAddress);
+            _mcast.setSoTimeout((int) _keepAliveInterval);
+            _runner=new Thread(this);
+            _runner.setName("MulticastDiscovery: "+_selfService);
+            _runner.setDaemon(true);
+            _runner.start();
+            doAdvertizeSelf();
+        }
+    }
+
+    /**
+     * stop the channel
+     * 
+     * @throws Exception
+     */
+    public void stop() throws Exception{
+        if(_started.compareAndSet(true,false)){
+            doAdvertizeSelf();
+            _mcast.close();
+        }
+    }
+
+    public String getType(){
+        return _group+"."+TYPE_SUFFIX;
+    }
+
+    public void run(){
+        byte[] buf=new byte[BUFF_SIZE];
+        DatagramPacket packet=new DatagramPacket(buf,0,buf.length);
+        while(_started.get()){
+            doTimeKeepingServices();
+            try{
+                _mcast.receive(packet);
+                if(packet.getLength()>0){
+                    String str=new String(packet.getData(),packet.getOffset(),packet.getLength());
+                    processData(str);
+                }
+            } catch(SocketTimeoutException se){
+                // ignore
+            } catch(IOException e){
+                if( _started.get() ) {
+                    log.error("failed to process packet: "+e);
+                }
+            }
+        }
+    }
+
+    private void processData(String str){
+        if (discoveryListener != null){
+        if(str.startsWith(getType())){
+            String payload=str.substring(getType().length());
+            if(payload.startsWith(ALIVE)){
+                String brokerName=getBrokerName(payload.substring(ALIVE.length()));
+                String service=payload.substring(ALIVE.length()+brokerName.length()+2);
+                if(!brokerName.equals(this._brokerName)){
+                    processAlive(brokerName,service);
+                }
+            }else{
+                String brokerName=getBrokerName(payload.substring(DEAD.length()));
+                String service=payload.substring(DEAD.length()+brokerName.length()+2);
+                if(!brokerName.equals(this._brokerName)){
+                    processDead(brokerName,service);
+                }
+            }
+        }
+        }
+    }
+
+    private void doTimeKeepingServices(){
+        if(_started.get()){
+            long currentTime=System.currentTimeMillis();
+            if (currentTime < _lastAdvertizeTime || ((currentTime-_keepAliveInterval)>_lastAdvertizeTime)) {
+                doAdvertizeSelf();
+                _lastAdvertizeTime = currentTime;
+            }
+            doExpireOldServices();
+        }
+    }
+
+    private void doAdvertizeSelf(){
+        if(_selfService!=null ){
+            String payload=getType();
+            payload+=_started.get()?ALIVE:DEAD;
+            payload+=DELIMITER+_brokerName+DELIMITER;
+            payload+=_selfService;
+            try{
+                byte[] data=payload.getBytes();
+                DatagramPacket packet=new DatagramPacket(data,0,data.length,_sockAddress);
+                _mcast.send(packet);
+            } catch(IOException e) {
+                // If a send fails, chances are all subsequent sends will fail too.. No need to keep reporting the
+                // same error over and over.
+                if( _reportAdvertizeFailed ) {
+                    _reportAdvertizeFailed=false;
+                    log.error("Failed to advertise our service: "+payload,e);
+                    if( "Operation not permitted".equals(e.getMessage()) ) {
+                        log.error("The 'Operation not permitted' error has been know to be caused by improper firewall/network setup.  Please make sure that the OS is properly configured to allow multicast traffic over: "+_mcast.getLocalAddress());
+                    }
+                }
+            }
+        }
+    }
+
+    private void processAlive(String brokerName,String service){
+        if(_selfService == null || !service.equals(_selfService)){
+            AtomicLong lastKeepAlive= _services.get(service);
+            if(lastKeepAlive==null){
+                _brokers.put(service, brokerName);
+                if(discoveryListener!=null){
+                    final DiscoveryEvent event=new DiscoveryEvent(service);
+                    event.setBrokerName(brokerName);
+                    
+                    // Have the listener process the event async so that 
+                    // he does not block this thread since we are doing time sensitive
+                    // processing of events.
+                    executor.execute(new Runnable() {
+                        public void run() {
+                            DiscoveryListener dl = MulticastDiscoveryAgent.this.discoveryListener;
+                            if(dl!=null){
+                                dl.onServiceAdd(event);
+                            }
+                        }
+                    });
+                }
+                lastKeepAlive=new AtomicLong(System.currentTimeMillis());
+                _services.put(service,lastKeepAlive);
+                doAdvertizeSelf();
+                
+            }
+            lastKeepAlive.set(System.currentTimeMillis());
+        }
+    }
+
+    private void processDead(String brokerName,String service){
+        if(!service.equals(_selfService)){
+            if(_services.remove(service)!=null){
+                _brokers.remove(service);
+                if(discoveryListener!=null){
+                    final DiscoveryEvent event=new DiscoveryEvent(service);
+                    event.setBrokerName(brokerName);
+                    
+                    // Have the listener process the event async so that 
+                    // he does not block this thread since we are doing time sensitive
+                    // processing of events.
+                    executor.execute(new Runnable() {
+                        public void run() {
+                            DiscoveryListener dl = MulticastDiscoveryAgent.this.discoveryListener;
+                            if(dl!=null){
+                                dl.onServiceRemove(event);
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+
+    private void doExpireOldServices(){
+        long expireTime=System.currentTimeMillis()-(_keepAliveInterval*HEARTBEAT_MISS_BEFORE_DEATH);
+        for(Iterator<Entry<String, AtomicLong>> i=_services.entrySet().iterator();i.hasNext();){
+            Map.Entry<String, AtomicLong> entry=i.next();
+            AtomicLong lastHeartBeat= entry.getValue();
+            if(lastHeartBeat.get() < expireTime){
+                String brokerName = _brokers.get(entry.getKey());
+                processDead(brokerName,entry.getKey().toString());
+            }
+        }
+    }
+    
+    private String getBrokerName(String str){
+        String result = null;
+        int start = str.indexOf(DELIMITER);
+        if (start >= 0 ){
+            int end = str.indexOf(DELIMITER,start+1);
+            result=str.substring(start+1, end);
+        }
+        return result;
+    }
+
+    public void serviceFailed(DiscoveryEvent event) {//throws IOException {
+        processDead(event.getBrokerName(), event.getServiceName());
+    }
 }
--- gateway/src/main/java/de/dailab/jiac/net/discovery/multicast/MulticastDiscoveryAgentFactory.java
+++ gateway/src/main/java/de/dailab/jiac/net/discovery/multicast/MulticastDiscoveryAgentFactory.java
@@ -5,22 +5,34 @@ package de.dailab.jiac.net.discovery.multicast;
 
 import java.io.IOException;
 import java.net.URI;
+import java.util.Map;
 
 import org.apache.activemq.transport.discovery.DiscoveryAgent;
 import org.apache.activemq.transport.discovery.DiscoveryAgentFactory;
+import org.apache.activemq.util.IOExceptionSupport;
+import org.apache.activemq.util.IntrospectionSupport;
+import org.apache.activemq.util.URISupport;
 
 /**
+ * This class is a clone of
+ * {@link org.apache.activemq.transport.discovery.multicast.MulticastDiscoveryAgentFactory}!
+ * 
  * @author Marcel Patzlaff
  * @version $Revision$
  */
 public class MulticastDiscoveryAgentFactory extends DiscoveryAgentFactory {
 
-    /* (non-Javadoc)
-     * @see org.apache.activemq.transport.discovery.DiscoveryAgentFactory#doCreateDiscoveryAgent(java.net.URI)
-     */
+    @SuppressWarnings("unchecked")
     protected DiscoveryAgent doCreateDiscoveryAgent(URI uri) throws IOException {
-        // TODO Auto-generated method stub
-        return null;
+        try {
+            Map options = URISupport.parseParamters(uri);
+            MulticastDiscoveryAgent rc = new MulticastDiscoveryAgent();
+            rc.setGroup(uri.getHost());
+            IntrospectionSupport.setProperties(rc, options);
+            return rc;
+            
+        } catch (Throwable e) {
+            throw IOExceptionSupport.create("Could not create discovery agent: " + uri, e);
+        }
     }
-
 }

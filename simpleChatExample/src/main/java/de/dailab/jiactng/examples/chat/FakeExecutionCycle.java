8146a420c01c729207d520f82f75edaa822d71fb
moekon <moekon@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2009-02-10 09:07:57 +0000

    CHANGE: some changes for automatic action invocation - not yet complete
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/examples@21941 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- simpleChatExample/src/main/java/de/dailab/jiactng/examples/chat/FakeExecutionCycle.java
+++ simpleChatExample/src/main/java/de/dailab/jiactng/examples/chat/FakeExecutionCycle.java
@@ -6,6 +6,7 @@ package de.dailab.jiactng.examples.chat;
 import de.dailab.jiactng.agentcore.AbstractAgentBean;
 import de.dailab.jiactng.agentcore.IAgent;
 import de.dailab.jiactng.agentcore.execution.IExecutionCycle;
+import java.util.List;
 
 /**
  * @author Marcel Patzlaff
@@ -19,4 +20,21 @@ public class FakeExecutionCycle extends AbstractAgentBean implements IExecutionC
     public void run() {
 
     }
+    
+    public void setAutoExecutionServices(List<String> actionIds) {
+      ;
+	  }
+	    
+	  public List<String> getAutoExecutionServices() {
+	    return null;
+	    
+	  }
+	    
+	  public void setAutoExecutionType(boolean continous) {
+	    ;
+	  }
+	    
+	  public boolean getAutoExecutionType() {
+	    return true;
+	  }
 }

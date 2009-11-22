d036ff0c6e1270b389ea8fc9afc02cf2ef31dcb1
keiser <keiser@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2009-11-22 20:00:14 +0000

    Publish action on node.
    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk/tools@25553 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

--- src/test/java/de/dailab/ccact/tools/agentunit/TestMethodExposingBean.java
+++ src/test/java/de/dailab/ccact/tools/agentunit/TestMethodExposingBean.java
@@ -3,11 +3,12 @@
 package de.dailab.ccact.tools.agentunit;
 
 import de.dailab.jiactng.agentcore.action.AbstractMethodExposingBean;
+import de.dailab.jiactng.agentcore.action.scope.ActionScope;
 
 
 public class TestMethodExposingBean extends AbstractMethodExposingBean {
 	
-	@Expose(name="testAction")
+	@Expose(name="testAction", scope=ActionScope.NODE)
 	public boolean testAction(){
 		return true;
 	}

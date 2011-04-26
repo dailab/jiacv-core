3f3f7f5e09be0d12cd056ca1ba7264ad508ec037
burkhardt <burkhardt@d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8>
2011-04-26 06:35:51 +0000

    git-svn-id: https://cvs.dai-labor.de/jiac/jiac-tng/trunk@32097 d8bac29e-b7f5-0310-9bdb-a06bcefdf3b8

---
+++ agentCoreAPI/src/main/java/de/dailab/jiactng/agentcore/execution/IPersistance.java
@@ -0,0 +1,17 @@
+package de.dailab.jiactng.agentcore.execution;
+
+/**
+ * This interface is used by an execution cycle to identify agent nodes with persistence implementation. This is need for
+ * distributed checkpointing (a.k.a. distributed snapshot).
+ * 
+ * @author mib
+ * @since AgentCore API 5.1.1
+ */
+/*
+ * This interface is designed for project 'ILIas' to create a recoverable infrastructure based on JIACv. 
+ */
+public interface IPersistance {
+	/*
+	 * add more methods, constants etc.
+	 */
+}

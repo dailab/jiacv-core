8744d55121a657e3db0f4a87c311aa63ed495b88
Michael Burkhardt <m.burkhardt@tu-berlin.de>
2015-10-26 10:57:43 +0100

    no idea, what I changed

--- simpleChatExample/src/main/java/de/dailab/jiactng/examples/SimpleChatUI.java
+++ simpleChatExample/src/main/java/de/dailab/jiactng/examples/SimpleChatUI.java
@@ -13,7 +13,7 @@ import javax.swing.SwingUtilities;
 
 /**
  * Simple chat UI for writing messages and displaying past messages.
- * 
+ *
  * @author kuester
  */
 public class SimpleChatUI extends JFrame {
@@ -21,45 +21,46 @@ public class SimpleChatUI extends JFrame {
 	private static final long serialVersionUID = 7968115785141545541L;
 
 	private static final String NL = System.getProperty("line.separator");
-	
+
 	/** Typed messages go here ... */
 	private final MessageHandler handler;
-	
+
 	/** ... and received messages go here */
 	private final JTextArea output;
-	
+
 	/**
 	 * Create new Instance of Simple Chat UI. Better use the helper method
 	 * {@link #createInstance(MessageHandler)} instead, for proper swing thread.
-	 * 
-	 * @param handler	the message handler
+	 *
+	 * @param handler
+	 *          the message handler
 	 */
-	private SimpleChatUI(MessageHandler handler) {
+	private SimpleChatUI(final MessageHandler handler) {
 		super("Simple JIAC Chat Exmaple UI");
-		
+
 		this.handler = handler;
-	
+
 		// create input text field
 		final JTextField input = new JTextField();
 		input.addActionListener(new ActionListener() {
 			@Override
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(final ActionEvent e) {
 				SimpleChatUI.this.handler.sendMessage(input.getText());
 				input.setText("");
 			}
 		});
 
 		// create output text area
-		output = new JTextArea();
-		output.setEditable(false);
+		this.output = new JTextArea();
+		this.output.setEditable(false);
 
 		// assemble widgets
 		this.getContentPane().setLayout(new BorderLayout());
-		this.getContentPane().add(new JScrollPane(output), BorderLayout.CENTER);
+		this.getContentPane().add(new JScrollPane(this.output), BorderLayout.CENTER);
 		this.getContentPane().add(input, BorderLayout.SOUTH);
 		this.pack();
 	}
-	
+
 	@Override
 	public Dimension getPreferredSize() {
 		return new Dimension(400, 300);
@@ -67,30 +68,32 @@ public class SimpleChatUI extends JFrame {
 
 	/**
 	 * Add a new message to be displayed in the UI's output panel.
-	 * 
+	 *
 	 * @param message
 	 */
-	public void addMessage(ChatMessage message) {
-		this.output.setText(this.output.getText() + formatMessage(message) + NL);
+	public void addMessage(final ChatMessage message) {
+		this.output.setText(this.output.getText() + this.formatMessage(message) + NL);
 	}
-	
+
 	/*
 	 * Return the message in usual chat message format.
 	 */
-	private String formatMessage(ChatMessage message) {
+	private String formatMessage(final ChatMessage message) {
 		return String.format("[%1$tH:%1$tM:%1$tS] %2$s: %3$s", message.sendtimestamp, message.sendername, message.message);
 	}
-	
+
 	/**
-	 * Create new instance of the UI given a message handler to relay messages to, 
+	 * Create new instance of the UI given a message handler to relay messages to,
 	 * start and display it using a swing thread, and return it.
-	 * 
-	 * @param handler	where to relay typed messages to
-	 * @return			new instance
+	 *
+	 * @param handler
+	 *          where to relay typed messages to
+	 * @return new instance
 	 */
-	public static SimpleChatUI createInstance(MessageHandler handler) {
+	public static SimpleChatUI createInstance(final MessageHandler handler) {
 		final SimpleChatUI frame = new SimpleChatUI(handler);
 		SwingUtilities.invokeLater(new Runnable() {
+			@Override
 			public void run() {
 				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 				frame.setVisible(true);
@@ -99,31 +102,31 @@ public class SimpleChatUI extends JFrame {
 		});
 		return frame;
 	}
-	
+
 	/**
-	 * Interface for handling new messages, to be implemented by SimpleChatAgentBean.
-	 * This callback is called whenever the user hits 'enter' in the input field.
+	 * Interface for handling new messages, to be implemented by
+	 * SimpleChatAgentBean. This callback is called whenever the user hits 'enter'
+	 * in the input field.
 	 *
 	 * @author kuester
 	 */
 	public interface MessageHandler {
-		
+
 		void sendMessage(String message);
-		
+
 	}
 
-	
 	/*
 	 * Testing...
 	 */
-	public static void main(String[] args) throws Exception {
+	public static void main(final String[] args) throws Exception {
 		final SimpleChatUI[] ui = new SimpleChatUI[1];
 		ui[0] = createInstance(new MessageHandler() {
 			@Override
-			public void sendMessage(String message) {
+			public void sendMessage(final String message) {
 				ui[0].addMessage(new ChatMessage("Name", "Message", System.currentTimeMillis()));
 			}
 		});
 	}
-	
+
 }

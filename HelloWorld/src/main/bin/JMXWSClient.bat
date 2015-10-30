9e0d0122d6232135742db793535eb7f9e0702324
Jan Keiser <jan.keiser@dai-labor.de>
2015-10-30 16:07:08 +0100

    Cleanup of the HelloWorld example: - added starter class - updated javadoc comments - removed class Client (JMX client) - changed from assembly plugin to appassembler plugin - removed java webstart (jnlp) - updated site and changes

--- HelloWorld/src/main/bin/JMXWSClient.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set LOCALCLASSPATH=
-for %%i in ("..\lib\*") do call "lcp.bat" %%i
-
-"%JAVA_HOME%\bin\java" -cp .;..\conf\;%LOCALCLASSPATH% de.dailab.jiactng.examples.helloWorld.Client 
-
-pause

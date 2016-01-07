861fbadd228550062e2e3e589ebffe3f155aaff2
kuester <tobias.kuester@dai-labor.de>
2016-01-07 14:35:57 +0100

    made test super class abstract so eclipse does not complain about empty unit test

--- agentCore/src/test/java/de/dailab/jiactng/JIACTestForJUnit3.java
+++ agentCore/src/test/java/de/dailab/jiactng/JIACTestForJUnit3.java
@@ -2,7 +2,7 @@ package de.dailab.jiactng;
 
 import junit.framework.TestCase;
 
-public class JIACTestForJUnit3 extends TestCase {
+public abstract class JIACTestForJUnit3 extends TestCase {
 
 	static {
 		System.setProperty("log4j.configuration", "jiactng_log4j.properties");

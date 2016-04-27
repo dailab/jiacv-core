d2b33786a87bcf454c1035e130d1dc1faa0ff699
kuester <tobias.kuester@dai-labor.de>
2016-04-27 14:07:46 +0200

    moved most of shared code of simple and nonblocking execution to new intermediate super class BasicExecutionCycle

--- agentCore/src/main/java/de/dailab/jiactng/agentcore/execution/NonBlockingExecutionCycle.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/execution/NonBlockingExecutionCycle.java
@@ -3,16 +3,11 @@ package de.dailab.jiactng.agentcore.execution;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.Future;
 
-import de.dailab.jiactng.agentcore.IAgentBean;
-import de.dailab.jiactng.agentcore.action.Action;
 import de.dailab.jiactng.agentcore.action.ActionResult;
 import de.dailab.jiactng.agentcore.action.DoAction;
-import de.dailab.jiactng.agentcore.action.Session;
-import de.dailab.jiactng.agentcore.action.SessionEvent;
 
 /**
  * A non-blocking ExecutionCycle implementation. This class executes active agentbeans
@@ -27,12 +22,9 @@ import de.dailab.jiactng.agentcore.action.SessionEvent;
  * @see DoAction
  * @see ActionResult
  */
-public class NonBlockingExecutionCycle extends AbstractExecutionCycle 
+public class NonBlockingExecutionCycle extends BasicExecutionCycle 
 	implements NonBlockingExecutionCycleMBean {
 
-	protected static final Session SESSION_TEMPLATE = new Session(null, null, null, null);
-	protected static final ActionResult ACTIONRESULT_TEMPLATE = new ActionResult(null, null);
-	protected static final DoAction DOACTION_TEMPLATE = new DoAction(null, null, null, null);
 	protected TreeMap<Long,List<Future<?>>> futures = new TreeMap<Long,List<Future<?>>>();
 
 	/**
@@ -80,6 +72,16 @@ public class NonBlockingExecutionCycle extends AbstractExecutionCycle
 		}
 	}
 
+	/** 
+	 * all execution cycle operations are performed in parallel using the agent's thread pool.
+	 */
+	@Override
+	protected void performExecutionCycleProcess(Runnable process) {
+		final Future<?> executionFuture = thisAgent.getThreadPool().submit(process);
+		addFuture(executionFuture);
+	}
+
+	
 	/**
 	 * {@inheritDoc}
 	 */
@@ -93,81 +95,6 @@ public class NonBlockingExecutionCycle extends AbstractExecutionCycle
 		}
 	}
 
-	/**
-	 * A handler for processing one bean execution within an own thread.
-	 * @author Jan Keiser
-	 * @see IAgentBean#execute()
-	 */
-	private class ExecutionHandler implements Runnable {
-		private IAgentBean minBean;
-
-		public ExecutionHandler(IAgentBean minBean) {
-			this.minBean = minBean;
-		}
-
-		public void run() {
-			try {
-				minBean.execute();
-			} catch (Exception ex) {
-				log.error("Error when executing bean \'"
-						+ minBean.getBeanName() + "\'", ex);
-			}
-		}
-	}
-
-	/**
-	 * A handler for processing one DoAction within an own thread.
-	 * @author Jan Keiser
-	 * @see AbstractExecutionCycle#performDoAction(DoAction)
-	 */
-	private class DoActionHandler implements Runnable {
-		private DoAction act;
-
-		public DoActionHandler(DoAction act) {
-			this.act = act;
-		}
-
-		public void run() {
-			performDoAction(act);
-		}
-	}
-
-	/**
-	 * A handler for processing one ActionResult within an own thread.
-	 * @author Jan Keiser
-	 * @see AbstractExecutionCycle#processResult(ActionResult)
-	 */
-	private class ActionResultHandler implements Runnable {
-		private ActionResult actionResult;
-
-		public ActionResultHandler(ActionResult actionResult) {
-			this.actionResult = actionResult;
-		}
-
-		public void run() {
-			processResult(actionResult);
-		}
-	}
-
-	/**
-	 * A handler for processing the cancellation of one DoAction within an own thread.
-	 * @author Jan Keiser
-	 * {@link de.dailab.jiactng.agentcore.environment.IEffector#cancelAction(DoAction)}
-	 */
-	private class SessionTimeoutHandler implements Runnable {
-		private Session session;
-		private DoAction doAction;
-
-		public SessionTimeoutHandler(Session session, DoAction doAction) {
-			this.session = session;
-			this.doAction = doAction;
-		}
-
-		public void run() {
-			processSessionTimeout(session, doAction);
-		}
-		
-	}
 
 	/* Adds a runnable thread to the internal data structure with the agent's bean execution timeout. */
 	private void addFuture(Future<?> executionFuture) {
@@ -180,88 +107,6 @@ public class NonBlockingExecutionCycle extends AbstractExecutionCycle
 			}
 			futureList.add(executionFuture);
 		}		
-	}
-
-	/**
-	 * Starts in an own thread the execution of the most urgent active bean, 
-	 * which execution interval is expired.
-	 * @see IAgentBean#getExecutionInterval()
-	 * @see IAgentBean#getNextExecutionTime()
-	 * @see ExecutionHandler
-	 * @see AbstractExecutionCycle#updateWorkload(int, boolean)
-	 */
-	protected void processBeanExecution() {
-		IAgentBean minBean = null;
-		long minExecutionTime = Long.MAX_VALUE;
-		long now = System.currentTimeMillis();
-		for (IAgentBean bean : thisAgent.getAgentBeans()) {
-			// check bean's state, if not started --> reject
-			if (bean.getState() != LifecycleStates.STARTED) {
-				continue;
-			}
-
-			// check if bean has cyclic behavior, if not --> reject
-			if (bean.getExecutionInterval() <= 0) {
-				continue;
-			}
-
-			// execution time not reached yet --> reject
-			if (bean.getNextExecutionTime() > now) {
-				continue;
-			}
-
-			// execution time is not minimum --> reject
-			if (bean.getNextExecutionTime() > minExecutionTime) {
-				continue;
-			}
-
-			minBean = bean;
-			minExecutionTime = bean.getNextExecutionTime();
-		}
-
-		// if there is a minBean then execute
-		boolean executionDone = false;
-		if (minBean != null) {
-			executionDone = true;
-			final Future<?> executionFuture = thisAgent.getThreadPool().submit(new ExecutionHandler(minBean));
-			addFuture(executionFuture);
-
-			// reschedule bean
-			minBean.setNextExecutionTime(now + minBean.getExecutionInterval());
-		}
-		updateWorkload(EXECUTION, executionDone);		
-	}
-
-	/**
-	 * Starts processing of a given DoAction within an own thread.
-	 * @param act the DoAction
-	 * @see DoActionHandler
-	 * @see AbstractExecutionCycle#updateWorkload(int, boolean)
-	 */
-	protected void processDoAction(DoAction act) {
-		boolean actionPerformed = false;
-		if (act != null) {
-			actionPerformed = true;
-			final Future<?> doActionFuture = thisAgent.getThreadPool().submit(new DoActionHandler(act));
-			addFuture(doActionFuture);
-		}
-		updateWorkload(DO_ACTION, actionPerformed);		
-	}
-
-	/**
-	 * Starts processing of a given ActionResult within an own thread.
-	 * @param result the ActionResult
-	 * @see ActionResultHandler
-	 * @see AbstractExecutionCycle#updateWorkload(int, boolean)
-	 */
-	protected void processActionResult(ActionResult result) {
-		boolean resultProcessed = false;
-		if (result != null) {
-			resultProcessed = true;
-			final Future<?> actionResultFuture = thisAgent.getThreadPool().submit(new ActionResultHandler(result));
-			addFuture(actionResultFuture);
-		}
-		updateWorkload(ACTION_RESULT, resultProcessed);
 	}
 
 	/**
@@ -302,47 +147,4 @@ public class NonBlockingExecutionCycle extends AbstractExecutionCycle
 		}		
 	}
 
-	/**
-	 * Starts canceling all DoActions, which session has a timeout.
-	 * @see Session#isTimeout()
-	 * @see Session#getHistory()
-	 * @see SessionTimeoutHandler
-	 */
-	protected void processSessionTimeouts() {
-		synchronized (memory) {
-			final Set<Session> sessions = memory.readAll(SESSION_TEMPLATE);
-			for (Session session : sessions){
-				if (session.isTimeout()){
-					// session has timeout
-					log.warn(TIMEOUT_MESSAGE + session);
-
-					final ArrayList<SessionEvent> history = session.getHistory();
-
-					// Does Session is related to DoAction?
-					boolean doActionFound = false;
-					for (SessionEvent event : history) {
-						if (event instanceof DoAction) {
-							// doAction found
-							doActionFound = true;
-							final DoAction doAction = (DoAction) event;
-							memory.remove(doAction);
-							
-							if (doAction.getAction() instanceof Action) {
-								// Got an Action, so let's cancel this doAction
-								final Future<?> sessionTimeoutFuture = thisAgent.getThreadPool().submit(
-										new SessionTimeoutHandler(session, doAction));
-								addFuture(sessionTimeoutFuture);
-							}
-						}
-					}
-
-					if (!doActionFound) {
-						log.warn("Session with no DoAction was deleted due to timeout. Session: " + session);
-					}
-					//last but not least remove timeout session from memory
-					memory.remove(session);
-				}
-			}
-		}		
-	}
 }
--- agentCore/src/main/java/de/dailab/jiactng/agentcore/execution/SimpleExecutionCycle.java
+++ agentCore/src/main/java/de/dailab/jiactng/agentcore/execution/SimpleExecutionCycle.java
@@ -6,14 +6,8 @@
  */
 package de.dailab.jiactng.agentcore.execution;
 
-import java.util.ArrayList;
-import java.util.Set;
-
-import de.dailab.jiactng.agentcore.IAgentBean;
 import de.dailab.jiactng.agentcore.action.ActionResult;
 import de.dailab.jiactng.agentcore.action.DoAction;
-import de.dailab.jiactng.agentcore.action.Session;
-import de.dailab.jiactng.agentcore.action.SessionEvent;
 
 /**
  * A simple ExecutionCycle implementation. This class executes active agentbeans (those agentbeans where the
@@ -27,14 +21,16 @@ import de.dailab.jiactng.agentcore.action.SessionEvent;
  * @see DoAction
  * @see ActionResult
  */
-public final class SimpleExecutionCycle extends AbstractExecutionCycle {
+public final class SimpleExecutionCycle extends BasicExecutionCycle {
 
-  private static final Session      SESSION_TEMPLATE      = new Session(null, null, null, null);
-
-  private static final ActionResult ACTIONRESULT_TEMPLATE = new ActionResult(null, null);
-
-  private static final DoAction     DOACTION_TEMPLATE     = new DoAction(null, null, null, null);
-
+	/**
+	 * execution cycle processes are exeuted directly, in the same thread.
+	 */
+	@Override
+	protected void performExecutionCycleProcess(Runnable process) {
+		process.run();
+	}
+	
   /**
    * Run-method for the execution cycle. The method iterates over the list of agentbeans and calls the execute method of
    * each <i>active</i> agentbean.
@@ -52,13 +48,15 @@ public final class SimpleExecutionCycle extends AbstractExecutionCycle {
     if (getState() == LifecycleStates.STARTED) {
 
       // execute one Beans execute Method
-      processBeanExecutes();
+      processBeanExecution();
 
       // process one DoAction
-      processDoActions();
+      final DoAction act = memory.remove(DOACTION_TEMPLATE);
+      processDoAction(act);
 
       // process one ActionResult
-      processActionResults();
+      final ActionResult result = memory.remove(ACTIONRESULT_TEMPLATE);
+      processActionResult(result);
 
       processSessionTimeouts();
       
@@ -69,127 +67,6 @@ public final class SimpleExecutionCycle extends AbstractExecutionCycle {
     // reject execution if SimpleExecutionCycle hasn't been started
     else {
       log.error("Trying to run SimpleExecutionCycle in state " + getState());
-    }
-  }
-
-  /**
-   * Find one bean whose execute Method is due and execute it.
-   */
-  private void processBeanExecutes() {
-    IAgentBean minBean = null;
-    long minExecutionTime = Long.MAX_VALUE;
-    final long now = System.currentTimeMillis();
-    for (IAgentBean bean : thisAgent.getAgentBeans()) {
-      // check bean's state, if not started --> reject
-      if (bean.getState() != LifecycleStates.STARTED) {
-        continue;
-      }
-
-      // check if bean has cyclic behavior, if not --> reject
-      if (bean.getExecutionInterval() <= 0) {
-        continue;
-      }
-
-      // execution time not reached yet --> reject
-      if (bean.getNextExecutionTime() > now) {
-        continue;
-      }
-
-      // execution time is not minimum --> reject
-      if (bean.getNextExecutionTime() > minExecutionTime) {
-        continue;
-      }
-
-      minBean = bean;
-      minExecutionTime = bean.getNextExecutionTime();
-    }
-
-    // if there is a minBean then execute
-    boolean executionDone = false;
-    if (minBean != null) {
-      executionDone = true;
-      try {
-        minBean.execute();
-      } catch (Exception ex) {
-        log.error("Error when executing bean \'" + minBean.getBeanName() + "\'", ex);
-      }
-
-      // reschedule bean
-      minBean.setNextExecutionTime(now + minBean.getExecutionInterval());
-    }
-
-    updateWorkload(EXECUTION, executionDone);
-  }
-
-  /**
-   * Process one DoAction from the Memory
-   */
-  private void processDoActions() {
-    final DoAction act = memory.remove(DOACTION_TEMPLATE);
-
-    boolean actionPerformed = false;
-    if (act != null) {
-      actionPerformed = true;
-      synchronized (this) {
-        performDoAction(act);
-      }
-    }
-    updateWorkload(DO_ACTION, actionPerformed);
-  }
-
-  /**
-   * Process one ActionResult from the Memory
-   */
-  private void processActionResults() {
-    final ActionResult result = memory.remove(ACTIONRESULT_TEMPLATE);
-    
-    boolean resultProcessed = false;
-    if(result != null) {
-      resultProcessed = true;
-      synchronized(this) {
-        processResult(result);
-      }
-    }
-    
-    updateWorkload(ACTION_RESULT, resultProcessed);
-  }
-
-  /**
-   * Session-Cleanup
-   * 
-   * If Session has a timeout
-   */
-  private void processSessionTimeouts() {
-    synchronized (memory) {
-      final Set<Session> sessions = memory.readAll(SESSION_TEMPLATE);
-      for (Session session : sessions) {
-        if (session.isTimeout()) {
-          // session has timeout
-          log.warn(TIMEOUT_MESSAGE + session);
-
-          final ArrayList<SessionEvent> history = session.getHistory();
-
-          // Does Session is related to DoAction?
-          boolean doActionFound = false;
-          for (SessionEvent event : history) {
-            if (event instanceof DoAction) {
-              // doAction found
-              doActionFound = true;
-              final DoAction doAction = (DoAction) event;
-              memory.remove(doAction);
-
-              processSessionTimeout(session, doAction);
-            }
-          }
-
-          if (!doActionFound) {
-            // Such a Session should not exist, but who knows... 
-            log.warn("Session with no DoAction was deleted due to timeout. Session: " + session);
-          }
-          // last but not least remove timeout session from memory
-          memory.remove(session);
-        }
-      }
     }
   }
 
